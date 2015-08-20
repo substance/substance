c84f9ba519aa1df85eed5d47fce79d99391ecf68
Michael Aufreiter <michael@substance.io>
2015-08-20 21:29:25 +0200

    Remember document for proper deregistration.

--- ui/nodes/link_component.js
+++ ui/nodes/link_component.js
@@ -14,17 +14,15 @@ var LinkComponent = AnnotationComponent.extend({
   didMount: function() {
     AnnotationComponent.prototype.didMount.call(this);
     var node = this.props.node;
-    var doc = node.getDocument();
-    doc.getEventProxy('path').add([node.id, 'title'], this, this.rerender);
-    doc.getEventProxy('path').add([node.id, 'url'], this, this.rerender);
+    this.doc = node.getDocument();
+    this.doc.getEventProxy('path').add([node.id, 'title'], this, this.rerender);
+    this.doc.getEventProxy('path').add([node.id, 'url'], this, this.rerender);
   },
 
   willUnmount: function() {
     AnnotationComponent.prototype.willUnmount.call(this);
-    var node = this.props.node;
-    var doc = node.getDocument();
-    doc.getEventProxy('path').remove([node.id, 'title'], this);
-    doc.getEventProxy('path').remove([node.id, 'url'], this);
+    this.doc.getEventProxy('path').remove([this.props.node.id, 'title'], this);
+    this.doc.getEventProxy('path').remove([this.props.node.id, 'url'], this);
   }
 
 });
