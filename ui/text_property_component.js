eff41c143f6dc2500045b54f8e5107b0773fdcf6
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-08-26 01:52:45 +0200

    Cleanup.

--- ui/text_property_component.js
+++ ui/text_property_component.js
@@ -31,35 +31,18 @@ TextPropertyComponent.Prototype = function() {
 
     var annotator = new Annotator();
     var fragmentCounters = {};
-    // for debugging
-    // var _level = 0;
-    // var _logIndent = function(level) {
-    //   var prefix = "";
-    //   for (var i = 0; i < level; i++) {
-    //     prefix = prefix.concat("  ");
-    //   }
-    //   return prefix;
-    // };
-    // var _logPrefix = "";
     annotator.onText = function(context, text) {
-      // console.log(_logPrefix+text);
       if (text && text.length > 0) {
         context.append(text);
       }
     };
     annotator.onEnter = function(entry) {
-      // for debugging
-      // _logPrefix = _logIndent(++_level);
       var node = entry.node;
       var id = node.id;
       if (!fragmentCounters[id]) {
         fragmentCounters[id] = 0;
       }
       fragmentCounters[id] = fragmentCounters[id]+1;
-      // for debugging
-      // console.log(_logPrefix+"<"+node.type+" key="+key+">");
-
-      // TODO: we need a component factory, so that we can create the appropriate component
       var ViewClass;
       if (componentRegistry.contains(node.type)) {
         ViewClass = componentRegistry.get(node.type);
@@ -75,7 +58,6 @@ TextPropertyComponent.Prototype = function() {
       // .key(id + "@" + fragmentCounters[id]);
       // special support for container annotation fragments
       if (node.type === "container_annotation_fragment") {
-        // TODO: this seems a bit messy
         el.addClass(node.anno.getTypeNames().join(' ').replace(/_/g, "-"));
         el.addClass("annotation-fragment");
       } else if (node.type === "container-annotation-anchor") {
@@ -86,9 +68,6 @@ TextPropertyComponent.Prototype = function() {
       return el;
     };
     annotator.onExit = function(entry, context, parentContext) {
-      // for debugging
-      // _logPrefix = _logIndent(_level--);
-      // console.log(_logPrefix+"</"+entry.node.type+">");
       parentContext.append(context);
     };
     annotator.start(el, text, annotations);
@@ -97,9 +76,6 @@ TextPropertyComponent.Prototype = function() {
     // as desired, and soft-breaks are not visible.
     // TODO: sometimes we do not want to do this. Make it configurable.
     el.append($$('br'));
-    // // HACK: need to post-process as some of the data has been
-    // // add using push and not processed via $$.
-    // Component.$$.prepareChildren(el.children);
     return el;
   };
 
