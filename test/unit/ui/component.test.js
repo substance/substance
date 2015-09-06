f5b64c5a2e2226abeb68bce72e9e644867c9d202
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-09-07 01:20:02 +0200

    Some more Substance.Component tests.

--- test/unit/ui/component.test.js
+++ test/unit/ui/component.test.js
@@ -7,7 +7,11 @@ QUnit.uiModule('Substance.Component');
 
 var SimpleComponent = Component.extend({
   render: function() {
-    return $$('div').addClass('simple-component');
+    var el = $$('div').addClass('simple-component');
+    if (this.props.children) {
+      el.append(this.props.children);
+    }
+    return el;
   }
 });
 
@@ -37,4 +41,38 @@ QUnit.test("Render a component", function(assert) {
   var comp = $$(SimpleComponent)._render();
   assert.equal(comp.$el[0].tagName.toLowerCase(), 'div', 'Element should be a "div".');
   assert.ok(comp.$el.hasClass('simple-component'), 'Element should have class "simple-component".');
+});
+
+QUnit.test("Render nested element", function(assert) {
+  var comp = $$('div').addClass('parent')
+    .append($$('div').addClass('child1'))
+    .append($$('div').addClass('child2'))
+    ._render();
+  assert.equal(comp.children.length, 2, 'Component should have two children.');
+  assert.ok(comp.$el.hasClass('parent'), 'Element should have class "parent".');
+  assert.ok(comp.children[0].$el.hasClass('child1'), 'First child should have class "child1".');
+  assert.ok(comp.children[1].$el.hasClass('child2'), 'Second child should have class "child2".');
+});
+
+QUnit.test("Render a component with children", function(assert) {
+  // Note: in case of custom components you should provide children via
+  // props, instead of using append. The render method of the custom
+  // needs to take care of 'placing' the children.
+  var comp = $$(SimpleComponent, {
+    children: [
+      $$('div').addClass('child1'),
+      $$('div').addClass('child2')
+    ]
+  })._render();
+  assert.equal(comp.children.length, 2, 'Component should have two children.');
+  assert.ok(comp.children[0].$el.hasClass('child1'), 'First child should have class "child1".');
+  assert.ok(comp.children[1].$el.hasClass('child2'), 'Second child should have class "child2".');
+});
+
+QUnit.test("Render a child with key", function(assert) {
+  var comp = $$('div').addClass('parent')
+    .append($$('div').addClass('child').key('foo'))
+    ._render();
+  assert.ok(comp.refs.foo, 'Element should have a ref "foo".');
+  assert.ok(comp.refs.foo.$el.hasClass('child'), 'Referenced component should have class "child".');
 });
