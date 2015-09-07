32bfd6340af8ef0c97f127942d50879cd2bf5163
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-09-07 03:40:39 +0200

    Fix typo.

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ app.get('/test/tmp/test.js', function (req, res, next) {
 app.use(express.static(__dirname));
 app.listen(PORT);
 console.log('Server is listening on %s', PORT);
-console.log('To run the test suite go to https://localhost:%s/test', PORT);
+console.log('To run the test suite go to http://localhost:%s/test', PORT);
