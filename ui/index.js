8f5b2e0ed869bc061ffe32a144030549bde1cc02
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-10-10 21:27:08 +0200

    Enabled api docs generation for Substance.UI

---
+++ ui/index.js
@@ -0,0 +1,20 @@
+/**
+ * Substance.UI
+ * ============
+ *
+ * TODO: add module description
+ *
+ * @module UI
+ */
+var UI = {};
+
+UI.Component = require('./component');
+UI.I18n = require('./i18n');
+
+UI.Controller = require('./controller');
+UI.Surface = require('./surface');
+UI.ContainerEditor = require('./container_editor');
+UI.TextPropertyComponent = require('./text_property_component');
+UI.ToolManager = require('./tool_manager');
+
+module.exports = UI;
