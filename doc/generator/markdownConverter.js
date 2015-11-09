d6c0c18758d51106730e0093508483ad89b903a1
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-09 02:57:46 +0100

    Support jsdoc cross links.

--- doc/generator/markdownConverter.js
+++ doc/generator/markdownConverter.js
@@ -8,6 +8,7 @@ var converter = {
   toHtml: function(text) {
     var parsed = reader.parse(text);
     highlightCodeblocks(parsed);
+    convertCodeLinks(parsed);
     return writer.render(parsed);
   }
 };
@@ -37,4 +38,42 @@ function highlightCodeblocks(parsed) {
     }
   }
 }
+
+function convertCodeLinks(parsed) {
+  var walker = parsed.walker();
+  var event, node;
+  var sourceposPre, sourceposLink, sourceposPost;
+  while ((event = walker.next())) {
+    node = event.node;
+    if (node.type === 'Text') {
+      var match = /(\{\s*@link([^\}]*)\})/.exec(node.literal);
+      if (match) {
+        sourceposPre = undefined;
+        sourceposLink = undefined;
+        sourceposPost = undefined;
+        if (node.sourcepos) {
+          var startLine = node.sourcepos[0][0];
+          var startCol = node.sourcepos[0][1];
+          var matchStart = startCol+match.index;
+          var matchEnd = matchStart+match[0].length;
+          sourceposPre = [node.sourcepos[0], [startLine, matchStart]];
+          sourceposLink = [[startLine, matchStart], [startLine, matchEnd]];
+          sourceposPost = [[startLine, matchEnd], node.sourcepos[1]];
+        }
+        var id = match[2].trim();
+        var pre = new commonmark.Node('Text', sourceposPre);
+        pre.literal = node.literal.slice(0, match.index);
+        var link = new commonmark.Node('Html', sourceposLink);
+        link.literal = ['<a href="#'+id+'" data-type="cross-link" data-refid="'+id+'">', id,'</a>'].join('');
+        var post = new commonmark.Node('Text', sourceposPost);
+        post.literal = node.literal.slice(match.index+match[0].length);
+        node.insertBefore(pre);
+        node.insertBefore(link);
+        node.insertBefore(post);
+        node.unlink();
+      }
+    }
+  }
+}
+
 module.exports = converter;
