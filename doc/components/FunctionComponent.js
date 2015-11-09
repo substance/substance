202e94774b9c3c71d15662cbb3045bdf8df70d19
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-09 13:25:37 +0100

    Show parameters in Function docs.

--- doc/components/FunctionComponent.js
+++ doc/components/FunctionComponent.js
@@ -7,6 +7,7 @@ var $$ = Component.$$;
 
 var Heading = require('./HeadingComponent');
 var Signature = require('./SignatureComponent');
+var Params = require('./ParamsComponent');
 var Example = require('./ExampleComponent');
 
 function FunctionComponent() {
@@ -17,20 +18,18 @@ FunctionComponent.Prototype = function() {
 
   this.render = function() {
     var node = this.props.node;
-
     // Constructor params
     var el = $$('div')
       .addClass('sc-function')
       .attr("data-id", node.id);
-
     // header
     el.append($$(Heading, {node: node}));
-
     //signature
     el.append($$(Signature, {node: node}));
-
     // description
     el.append($$('div').addClass('se-description').html(node.description));
+    // params
+    el.append($$(Params, {params: node.params, returns: node.returns}));
     // example
     if (node.example) {
       el.append($$(Example, {node: node}));
