aa343c8502a4bdc72d624bc6e7dc3b45e4f5feac
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-12 18:05:39 +0100

    Don't render member index if there are no members.

--- doc/components/ModuleComponent.js
+++ doc/components/ModuleComponent.js
@@ -7,6 +7,7 @@ var $$ = Component.$$;
 var DocumentationNodeComponent = require('./DocumentationNodeComponent');
 var Heading = require('./HeadingComponent');
 var Example = require('./ExampleComponent');
+var MemberIndexComponent = require('./MemberIndexComponent');
 
 function ModuleComponent() {
   DocumentationNodeComponent.apply(this, arguments);
@@ -15,10 +16,11 @@ function ModuleComponent() {
 ModuleComponent.Prototype = function() {
 
   this.render = function() {
+
     var node = this.props.node;
     var el = $$('div')
       .addClass('sc-module')
-      .attr("data-id", node.id)
+      .attr("data-id", node.id);
 
     // heading
     el.append($$(Heading, {node: node}));
@@ -26,13 +28,19 @@ ModuleComponent.Prototype = function() {
     el.append(
       $$('div').addClass('se-description').html(node.description)
     );
-    // members
-    el.append(
-      $$('div').addClass('se-members').append(this._renderMembers())
-    );
     // example
     if (node.example) {
       el.append($$(Example, {node:node}));
+    }
+
+    // members
+    if (node.members && node.members.length > 0) {
+      // member index
+      el.append($$(MemberIndexComponent, {node: node}));
+      // members
+      el.append(
+        $$('div').addClass('se-members').append(this._renderMembers())
+      );
     }
 
     return el;
