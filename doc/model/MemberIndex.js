06b2e76536f12c4b747c369135b158e8e0a652d7
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-12 18:09:30 +0100

    Introduced MemberIndex.
    A NodeIndex which groups nodes by parent id, 'class' vs 'instance', and type.
    I.e. ['ui/Component', 'class', 'method', '$$'] contains the documentation
    node of ui/Component.$$.

---
+++ doc/model/MemberIndex.js
@@ -0,0 +1,72 @@
+
+var oo = require('../../util/oo');
+var NodeIndex = require('../../model/data/NodeIndex');
+var PathAdapter = require('../../util/PathAdapter');
+
+function MemberIndex() {
+  NodeIndex.apply(this, arguments);
+
+  this.index = new PathAdapter();
+}
+
+MemberIndex.Prototype = function() {
+
+  /**
+   * Selects all nodes which have a parent.
+   *
+   * @private
+   * @param {model/data/Node}
+   * @returns {Boolean} true if the given node should be added to the index.
+   */
+  this.select = function(node) {
+    return node.hasParent();
+  };
+
+  function _getPath(node, parentId) {
+    var parentId = parentId || node.parent;
+    var type = node.type;
+
+    if (node.isStatic) {
+      return [parentId, 'class', type, node.name];
+    } else {
+      return [parentId, 'instance', type, node.name];
+    }
+  };
+
+  /**
+   * Called when a node has been created.
+   *
+   * @private
+   * @param {Node} node
+   */
+  this.create = function(node) {
+    this.index.set(_getPath(node), node);
+  };
+
+  /**
+   * Called when a node has been deleted.
+   *
+   * @private
+   * @param {Node} node
+   */
+  this.delete = function(node) {
+    this.index.delete(_getPath(node));
+  };
+
+  /**
+   * Called when a property has been updated.
+   *
+   * @private
+   * @param {Node} node
+   */
+  this.update = function(node, path, newValue, oldValue) {
+    if (!this.select(node) || path[1] !== 'parent') return;
+    this.index.delete(_getPath(node, oldValue));
+    this.index.set(_getPath(node, newValue), node);
+  };
+
+};
+
+oo.inherit(MemberIndex, NodeIndex);
+
+module.exports = MemberIndex;
