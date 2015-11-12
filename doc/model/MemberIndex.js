fa0f2c4b8cc2c7056ae13f13adda2d42f36a20a8
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-12 18:36:06 +0100

    Clean up.

--- doc/model/MemberIndex.js
+++ doc/model/MemberIndex.js
@@ -3,6 +3,9 @@ var oo = require('../../util/oo');
 var NodeIndex = require('../../model/data/NodeIndex');
 var PathAdapter = require('../../util/PathAdapter');
 
+/**
+  @class
+ */
 function MemberIndex() {
   NodeIndex.apply(this, arguments);
 
@@ -12,11 +15,11 @@ function MemberIndex() {
 MemberIndex.Prototype = function() {
 
   /**
-   * Selects all nodes which have a parent.
-   *
-   * @private
-   * @param {model/data/Node}
-   * @returns {Boolean} true if the given node should be added to the index.
+    Selects all nodes which have a parent.
+
+    @private
+    @param {model/data/Node}
+    @returns {Boolean} true if the given node should be added to the index.
    */
   this.select = function(node) {
     return node.hasParent();
@@ -34,30 +37,30 @@ MemberIndex.Prototype = function() {
   };
 
   /**
-   * Called when a node has been created.
-   *
-   * @private
-   * @param {Node} node
+    Called when a node has been created.
+
+    @private
+    @param {Node} node
    */
   this.create = function(node) {
     this.index.set(_getPath(node), node);
   };
 
   /**
-   * Called when a node has been deleted.
-   *
-   * @private
-   * @param {Node} node
+    Called when a node has been deleted.
+
+    @private
+    @param {Node} node
    */
   this.delete = function(node) {
     this.index.delete(_getPath(node));
   };
 
   /**
-   * Called when a property has been updated.
-   *
-   * @private
-   * @param {Node} node
+    Called when a property has been updated.
+
+    @private
+    @param {Node} node
    */
   this.update = function(node, path, newValue, oldValue) {
     if (!this.select(node) || path[1] !== 'parent') return;
