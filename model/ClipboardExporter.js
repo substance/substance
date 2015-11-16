4e4804b1e544af81fabe2c8e1aee210fc4abd394
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-16 04:52:54 +0100

    Fix regressions.

--- model/ClipboardExporter.js
+++ model/ClipboardExporter.js
@@ -1,31 +1,18 @@
 "use strict";
 
 var oo = require('../util/oo');
-var HtmlExporter = require('./HtmlExporter');
+var HtmlExporter = require('./HTMLExporter');
 var converters = require('./ClipboardImporter').converters;
 
+// FIXME: this is not working yet
 function ClipboardExporter() {
   ClipboardExporter.super.call(this, {
-    converters: converters
+    converters: converters,
+    containerId: 'fixme'
   });
 }
 
 ClipboardExporter.Prototype = function() {
-
-
-  this.convert = function(doc, options) {
-    this.initialize(doc, options);
-    var $doc = this.createHtmlDocument();
-    // Note: the content of a clipboard document
-    // is coming as container with id 'clipboard'
-    var content = doc.get('clipboard_content');
-    $doc.find('body').append(this.convertContainer(content));
-
-    // This is not working with jquery
-    //return $doc.html();
-
-    return $doc.find('html').html();
-  };
 
 };
 
