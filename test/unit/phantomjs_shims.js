077025b295af7d599561f81d0211d6353c48b06a
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-18 11:34:18 +0100

    Restructured phantomjs polyfills.

--- test/unit/phantomjs_shims.js
+++ test/unit/phantomjs_shims.js
@@ -1,16 +1,1 @@
-if (typeof window !== 'undefined') {
-  if (!window.MutationObserver) {
-    window.MutationObserver = function() {
-      this.observe = function() {};
-      this.disconnect = function() {};
-    };
-  }
-  if (!Function.prototype.bind) {
-    Function.prototype.bind = Function.prototype.bind || function (thisp) {
-      var fn = this;
-      return function () {
-        return fn.apply(thisp, arguments);
-      };
-    };
-  }
-}
+require('../phantomjs/shims');
