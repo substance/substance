1a8ff6a14e7fe158dc023f5b0d86723335d2a632
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-19 22:38:21 +0100

    Extended Class.extend to also take over what oo.inherit does.

--- test/unit/util/oo.test.js
+++ test/unit/util/oo.test.js
@@ -4,40 +4,106 @@ var oo = require('../../../util/oo');
 
 QUnit.module('util/oo');
 
-QUnit.test("oo.extend and oo.inherit should propagate static properties", function(assert) {
-  function A() {}
-  oo.makeExtensible(A, {'name': true});
+function A() {}
+oo.initClass(A);
+A.prototype.bla = function() {
+  return "bla";
+};
 
-  var B = A.extend({
-    name: "foo"
-  });
-  assert.equal(B.static.name, 'foo', 'B should have static name "foo".');
-
-  function C() {}
-  C.Prototype = function() {
-    this.name = "foo";
-  };
-  oo.inherit(C, A);
-  assert.equal(C.static.name, 'foo', 'C should have static name "foo".');
+QUnit.test("Class.extend without a protoype", function(assert) {
+  var B = A.extend();
+  var b = new B();
+  assert.equal(b.bla(), 'bla', 'B should have inherited method A#bla().');
 });
 
-QUnit.test("oo.extend and oo.inherit should use proto.static", function(assert) {
-  function A() {}
-  oo.makeExtensible(A);
-
+QUnit.test("Class.extend with a protoype object", function(assert) {
   var B = A.extend({
-    static: {
-      foo: 'bar'
+    blupp: function() {
+      return "blupp";
     }
   });
-  assert.equal(B.static.foo, 'bar', 'B should have static property "foo".');
+  var b = new B();
+  assert.equal(b.blupp(), 'blupp', 'B should have a prototype function blupp().');
+});
 
-  function C() {}
-  C.Prototype = function() {
-    this.static = {
-      foo: "bar"
+QUnit.test("Class.extend with static properties", function(assert) {
+  var B = A.extend({
+    static: {
+      name: "foo"
+    }
+  });
+  assert.equal(B.static.name, 'foo', 'B should have a static property "name".');
+});
+
+QUnit.test("Class.extend with a ChildClass", function(assert) {
+  function B() {}
+  A.extend(B);
+  var b = new B();
+  assert.equal(b.bla(), 'bla', 'B should have inherited method A#bla().');
+});
+
+QUnit.test("Class.extend with a ChildClass and a prototype object", function(assert) {
+  function B() {}
+  A.extend(B, {
+    blupp: function() {
+      return "blupp";
+    }
+  });
+  var b = new B();
+  assert.equal(b.blupp(), 'blupp', 'B should have a prototype function blupp().');
+});
+
+QUnit.test("Class.extend with a ChildClass and a Prototype function", function(assert) {
+  function B() {}
+  A.extend(B, function() {
+    this.blupp = function() {
+      return "blupp";
+    };
+  });
+  var b = new B();
+  assert.equal(b.blupp(), 'blupp', 'B should have a prototype function blupp().');
+});
+
+QUnit.test("Legacy: Class.extend with a ChildClass with ChildClass.Prototype", function(assert) {
+  function B() {}
+  B.Prototype = function() {
+    this.blupp = function() {
+      return "blupp";
     };
   };
-  oo.inherit(C, A);
-  assert.equal(C.static.foo, 'bar', 'C should have static property "foo".');
+  A.extend(B);
+  var b = new B();
+  assert.equal(b.blupp(), 'blupp', 'B should have a prototype function blupp().');
+});
+
+QUnit.test("Class.extend with a ChildClass and mixins.", function(assert) {
+  function B() {}
+  var MixinA = {
+    foo: "foo"
+  };
+  var MixinB = {
+    bar: "bar"
+  };
+  A.extend(B, MixinA, MixinB);
+  var b = new B();
+  assert.equal(b.foo, 'foo', 'b should have a prototype property foo.');
+  assert.equal(b.bar, 'bar', 'b should have a prototype property bar.');
+});
+
+QUnit.test("Class.extend with ChildClass, mixins, and a prototype function.", function(assert) {
+  function B() {}
+  var MixinA = {
+    foo: "foo",
+    blupp: function() {
+      return "This should not be mixed in";
+    }
+  };
+  A.extend(B, MixinA, function() {
+    this.blupp = function() {
+      return "blupp";
+    };
+  });
+  var b = new B();
+  assert.equal(b.foo, 'foo', 'b should have a prototype property foo.');
+  assert.equal(b.blupp(), 'blupp', 'b should have a prototype method blupp() coming from the Prototype function.');
 });
--- util/oo.js
+++ util/oo.js
@@ -2,8 +2,8 @@
 
 'use strict';
 
-var extend = require('lodash/object/extend');
-var bind = require('lodash/function/bind');
+var isObject = require('lodash/lang/isObject');
+var isFunction = require('lodash/lang/isFunction');
 
 /**
  * Helpers for oo programming.
@@ -12,8 +12,6 @@ var bind = require('lodash/function/bind');
  */
 var oo = {};
 
-var defaultStaticProps = {'name': true, 'displayName': true};
-
 /**
  * Initialize a class.
  *
@@ -21,7 +19,7 @@ var defaultStaticProps = {'name': true, 'displayName': true};
  */
 oo.initClass = function(clazz) {
   _initClass(clazz);
-  _makeExtensible(clazz, defaultStaticProps);
+  _makeExtensible(clazz);
 };
 
 /**
@@ -49,10 +47,22 @@ oo.inherit =  function(clazz, parentClazz) {
     oo.initClass(clazz);
   }
   _inherit(clazz, parentClazz);
-  if (clazz.static._afterClassInitHook) {
-    clazz.static._afterClassInitHook(clazz);
-  }
+  _afterClassInitHook(clazz);
 };
+
+function _afterClassInitHook(childClazz) {
+  var proto = childClazz.prototype;
+  for(var key in proto) {
+    if (key === "constructor" ||
+        key === "__class__" ||
+        !proto.hasOwnProperty(key)) continue;
+    // built-in: extend class.static with prototype.static
+    if (key === 'static') {
+      _copyStaticProps(childClazz.static, proto.static);
+      continue;
+    }
+  }
+}
 
 /*
  * @param clazz {Constructor} class constructor
@@ -94,25 +104,24 @@ function _initClass(clazz) {
   clazz.__is_initialized__ = true;
 }
 
-function _inherit(clazz, parentClazz) {
-  if (clazz.prototype instanceof parentClazz) {
+function _inherit(ChildClass, ParentClass) {
+  if (ChildClass.prototype instanceof ParentClass) {
     throw new Error('Target already inherits from origin');
   }
-  var targetConstructor = clazz.prototype.constructor;
   // Customization: supporting a prototype constructor function
   // defined as a static member 'Prototype' of the target function.
-  var TargetPrototypeCtor = clazz.Prototype;
+  var PrototypeCtor = ChildClass.Prototype;
   // Provide a shortcut to the parent constructor
-  clazz.super = parentClazz;
-  if (TargetPrototypeCtor) {
-    TargetPrototypeCtor.prototype = parentClazz.prototype;
-    clazz.prototype = new TargetPrototypeCtor();
-    clazz.prototype.constructor = clazz;
+  ChildClass.super = ParentClass;
+  if (PrototypeCtor) {
+    PrototypeCtor.prototype = ParentClass.prototype;
+    ChildClass.prototype = new PrototypeCtor();
+    ChildClass.prototype.constructor = ChildClass;
   } else {
-    clazz.prototype = Object.create(parentClazz.prototype, {
+    ChildClass.prototype = Object.create(ParentClass.prototype, {
       // Restore constructor property of clazz
       constructor: {
-        value: targetConstructor,
+        value: ChildClass,
         enumerable: false,
         writable: true,
         configurable: true
@@ -120,19 +129,115 @@ function _inherit(clazz, parentClazz) {
     });
   }
   // provide a shortcut to the parent prototype
-  clazz.prototype.super = parentClazz.prototype;
+  ChildClass.prototype.super = ParentClass.prototype;
   // Extend static properties - always initialize both sides
   var StaticScope = _StaticScope();
-  if (parentClazz.static) {
-    StaticScope.prototype = parentClazz.static;
+  if (ParentClass.static) {
+    StaticScope.prototype = ParentClass.static;
   }
-  clazz.static = new StaticScope(clazz);
-  if (clazz.static._makeExtendFunction) {
-    clazz.extend = clazz.static._makeExtendFunction(clazz);
+  ChildClass.static = new StaticScope(ChildClass);
+  if (ChildClass.static._makeExtendFunction) {
+    ChildClass.extend = ChildClass.static._makeExtendFunction(ChildClass);
   } else {
-    _makeExtensible(clazz, defaultStaticProps);
+    _makeExtensible(ChildClass);
   }
 }
+
+/*
+  Parent.extend([...mixins], [ChildClass], [prototype|Prototype])
+
+  Parent.extend(Child) -> (Function)
+  Parent.extend(EventEmitter, Child) -> (..., Function)
+  Parent.extend(Child, ChildPrototype)
+  Parent.extend(EventEmitter, Child, ChildPrototype)
+
+*/
+function _extendClass(ParentClass) {
+
+  // this ctor is used when extend is not provided with a constructor function.
+  function AnonymousClass() {
+    ParentClass.apply(this, arguments);
+  }
+
+  /*
+    extend() -> lazy inheritance without a proto
+    extend({...}) -> lazy inheritance with a proto
+    extend(Function) -> inheritance without a proto
+    extend(Function, {}) -> inherit with a proto
+    extend(Function, Function) -> inheritance with prototype function
+  */
+
+  var args = Array.prototype.slice.call(arguments, 1);
+  //var childOrProto = args[args.length-1];
+  var ChildClass;
+  var mixins = [];
+
+  // the first argument must be a Class constructor, otherwise we will use an anonymous ctor.
+  var idx = 0;
+  if (isFunction(args[0])) {
+    ChildClass = args[0];
+    idx++;
+  } else {
+    ChildClass = AnonymousClass;
+  }
+  // the remaining arguments should be Objects used as a mixin for the created prototype
+  // the last argument may be a prototype constructor function.
+  for (; idx < args.length; idx++) {
+    if (isFunction(args[idx])) {
+      if (ChildClass.hasOwnProperty('Prototype')) {
+        throw new Error('Class ' + ChildClass.name + ' has defined ' + ChildClass.name +
+         '.Prototype which would be overwritten by Class.extend().\n' +
+         'You provided a prototype function when calling Class.extend().');
+      } else {
+        ChildClass.Prototype = args[idx];
+      }
+      if (idx < args.length-1) {
+        throw new Error('Illegal use of Class.extend(): Prototype function must be last argument.');
+      }
+      break;
+    } else if (isObject(args[idx])) {
+      mixins.push(args[idx]);
+    } else {
+      throw new Error('Illegal use of Class.extend');
+    }
+  }
+  _inherit(ChildClass, ParentClass);
+
+  // from right to left copy all mixins into the prototype
+  // but never overwrite
+  // in like with lodash extend, the more right in the args list the more important
+  var proto = ChildClass.prototype;
+  for (var i = mixins.length - 1; i >= 0; i--) {
+    var mixin = mixins[i];
+    for(var key in mixin) {
+      if (mixin.hasOwnProperty(key)) {
+        // built-in: extend class.static with prototype.static
+        if (key === 'static') {
+          _copyStaticProps(ChildClass.static, mixin.static);
+          continue;
+        } else {
+          if (!proto.hasOwnProperty(key)) {
+            proto[key] = mixin[key];
+          }
+        }
+      }
+    }
+  }
+
+  return ChildClass;
+}
+
+function _makeExtensible(clazz) {
+  if (!clazz.static) {
+    oo.initClass(clazz);
+  }
+  clazz.static._makeExtendFunction = function(parentClazz) {
+    return _extendClass.bind(null, parentClazz);
+  };
+  clazz.extend = clazz.static._makeExtendFunction(clazz);
+}
+
+oo.makeExtensible = _makeExtensible;
 
 function _StaticScope() {
   return function StaticScope(clazz) {
@@ -152,85 +257,5 @@ function _copyStaticProps(staticProps, parentStaticProps) {
     staticProps[key] = parentStaticProps[key];
   }
 }
-
-function _extendClass(parent, staticProps, afterHook, proto) {
-  if (arguments.length > 4) {
-    var args = Array.prototype.slice.call(arguments, 3);
-    args.unshift({});
-    proto = extend.apply(null, args);
-  } else if (arguments.length === 3) {
-    proto = {};
-  }
-  var Constructor = function ExtendedClass() {
-    // this.__className__ = proto.displayName || proto.name;
-    parent.apply(this, arguments);
-    if (this.init) {
-      console.log('DEPRECATED: we want to drop this built-in hook in favor of a custom hook.');
-      this.init.apply(this, arguments);
-    }
-  };
-  if (proto.displayName) {
-    Constructor.displayName = proto.displayName;
-  } else if (proto.name) {
-    Constructor.displayName = proto.name;
-  }
-  // calling 'silently' so that afterHook doesn't get called too early
-  _inherit(Constructor, parent);
-  for(var key in proto) {
-    if (proto.hasOwnProperty(key)) {
-      // built-in: extend class.static with prototype.static
-      if (key === 'static') {
-        extend(Constructor.static, proto.static);
-        continue;
-      }
-      if (key in staticProps) {
-        Constructor.static[key] = proto[key];
-      } else {
-        Constructor.prototype[key] = proto[key];
-      }
-    }
-  }
-  if (afterHook) {
-    afterHook(Constructor, proto);
-  }
-  return Constructor;
-}
-
-function _makeExtensible(clazz, staticProps, afterHook) {
-  staticProps = staticProps || {};
-  if (!clazz.static) {
-    oo.initClass(clazz);
-  }
-  clazz.static._makeExtendFunction = function(parentClazz) {
-    return bind(_extendClass, null, parentClazz, staticProps, afterHook);
-  };
-  // add a hook that moves static properties to clazz.static
-  clazz.static._afterClassInitHook = function(childClazz) {
-    var proto = childClazz.prototype;
-    for(var key in proto) {
-      if (key === "constructor" ||
-          key === "__class__" ||
-          !proto.hasOwnProperty(key)) continue;
-      // built-in: extend class.static with prototype.static
-      if (key === 'static') {
-        _copyStaticProps(childClazz.static, proto.static);
-        continue;
-      }
-      // move over all static properties to class.static
-      if (proto.hasOwnProperty(key)) {
-        if (key in staticProps) {
-          childClazz.static[key] = proto[key];
-        }
-      }
-    }
-    if (afterHook) {
-      afterHook(childClazz);
-    }
-  };
-  clazz.extend = clazz.static._makeExtendFunction(clazz);
-}
-
-oo.makeExtensible = _makeExtensible;
-
 
 module.exports = oo;
