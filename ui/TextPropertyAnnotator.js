7d8cdb7f1ee4fff40cdb97001fe19f52cd1c4e9c
Michael Aufreiter <michael@substance.io>
2015-11-23 21:18:00 +0100

    Document TextPropertyAnnotator.

--- ui/TextPropertyAnnotator.js
+++ ui/TextPropertyAnnotator.js
@@ -8,11 +8,28 @@ var TextProperty = require('./TextPropertyComponent');
 var $$ = Component.$$;
 
 /**
- * TextPropertyAnnotator
- *
- * @memberof module:ui
- * @extends module:ui/Surface
- */
+  Annotator for a text property. Needs to be instantiated inside a {@link ui/Controller}
+  context. Works like a TextPropertyEditor but you can only annotate, not edit.
+
+  @class
+  @component
+  @extends ui/Surface
+  
+  @prop {String} name unique surface name
+  @prop {String[]} path path to a text property
+  @prop {ui/SurfaceCommand[]} commands array of command classes to be available
+
+  @example
+  
+  ```js
+  $$(TextPropertyAnnotator, {
+    name: 'abstract',
+    path: ['metadata', 'abstract'],
+    commands: [EmphasisCommand]
+  })
+  ```
+*/
+
 function TextPropertyAnnotator() {
   Surface.apply(this, arguments);
   var doc = this.getDocument();
