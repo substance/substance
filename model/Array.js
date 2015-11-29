f374fb0f6d40403c97927f4dce97e92c89718998
Daniel Beilinson <beilinson@gmail.com>
2015-11-29 22:47:06 +0300

    spaces

--- model/Array.js
+++ model/Array.js
@@ -8,33 +8,33 @@ function ArrayUpdater() {
 
 ArrayUpdater.Prototype = function() {
 
-	this.insert = function(tx, path, offset, value) {
+  this.insert = function(tx, path, offset, value) {
     if (isArray(oldValue)) {
-    	var diff = { insert: { offset: offset, value: value } }
+      var diff = { insert: { offset: offset, value: value } }
       var op = tx.data.update(path, diff);
       if (!op) return;
-	    if (tx.document.isTransacting) {
-	      tx.ops.push(op);
-	    }
-	    return op;
+      if (tx.document.isTransacting) {
+        tx.ops.push(op);
+      }
+      return op;
     } else {
       throw new Error('Value type is not supported:', JSON.stringify(value));
     }
-	}
+  }
 
-	this.delete = function(tx, path, offset) {
-		if (isArray(oldValue)) {
-    	var diff = { delete: { offset: offset } }
+  this.delete = function(tx, path, offset) {
+    if (isArray(oldValue)) {
+      var diff = { delete: { offset: offset } }
       var op = tx.data.update(path, diff);
       if (!op) return;
-	    if (tx.document.isTransacting) {
-	      tx.ops.push(op);
-	    }
-	    return op;
+      if (tx.document.isTransacting) {
+        tx.ops.push(op);
+      }
+      return op;
     } else {
       throw new Error('Value type is not supported:', JSON.stringify(value));
     }
-	}
+  }
 };
 
 module.exports = ArrayUpdater;
