cec5a514a3bb1e3d7ad6e26f24bb5ca751878b3e
Daniel Beilinson <beilinson@gmail.com>
2015-11-29 22:46:07 +0300

    array updater

---
+++ model/Array.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var isArray = require('lodash/lang/isArray');
+
+function ArrayUpdater() {
+  ArrayUpdater.super.apply(this, arguments);
+}
+
+ArrayUpdater.Prototype = function() {
+
+	this.insert = function(tx, path, offset, value) {
+    if (isArray(oldValue)) {
+    	var diff = { insert: { offset: offset, value: value } }
+      var op = tx.data.update(path, diff);
+      if (!op) return;
+	    if (tx.document.isTransacting) {
+	      tx.ops.push(op);
+	    }
+	    return op;
+    } else {
+      throw new Error('Value type is not supported:', JSON.stringify(value));
+    }
+	}
+
+	this.delete = function(tx, path, offset) {
+		if (isArray(oldValue)) {
+    	var diff = { delete: { offset: offset } }
+      var op = tx.data.update(path, diff);
+      if (!op) return;
+	    if (tx.document.isTransacting) {
+	      tx.ops.push(op);
+	    }
+	    return op;
+    } else {
+      throw new Error('Value type is not supported:', JSON.stringify(value));
+    }
+	}
+};
+
+module.exports = ArrayUpdater;
