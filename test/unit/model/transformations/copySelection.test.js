04778cd246f8aac20b5c31949d3f80fe004248fa
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-01-15 13:53:11 +0100

    Don't skip nodes without addresses.
    Fixes #392

--- test/unit/model/transformations/copySelection.test.js
+++ test/unit/model/transformations/copySelection.test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 require('../../qunit_extensions');
+var simple = require('../../../fixtures/simple');
 var sample1 = require('../../../fixtures/sample1');
 var copySelection = require('../../../../model/transform/copySelection');
 var CLIPBOARD_PROPERTY_ID = copySelection.CLIPBOARD_PROPERTY_ID;
@@ -57,6 +58,7 @@ QUnit.test("Copying a container selection", function(assert) {
   var copy = out.doc;
   var content = copy.get('clipboard_content');
   assert.isDefinedAndNotNull(content, 'There should be a container node with id "content".');
+  // 4 nodes? 'main', 'clipboard_content', 'p1', 'p2'
   assert.equal(content.nodes.length, 4, 'There should be 4 nodes in the copied document.');
   var first = copy.get(content.nodes[0]);
   assert.equal(first.type, 'heading', "The first node should be a heading.");
@@ -64,4 +66,27 @@ QUnit.test("Copying a container selection", function(assert) {
   var last = copy.get(content.nodes[3]);
   assert.equal(last.type, 'paragraph', "The last node should be a paragraph.");
   assert.equal(last.content, 'Paragraph', "Its content should be truncated to 'Paragraph'.");
+});
+
+QUnit.test("Copying a node without editable properties", function(assert) {
+  var doc = simple();
+  doc.create({
+    type: 'image',
+    id: 'i1',
+    src: 'foo'
+  });
+  doc.get('main').show('i1', 1);
+  var sel = doc.createSelection({
+    type: 'container',
+    containerId: 'main',
+    startPath: ['p1', 'content'],
+    startOffset: 4,
+    endPath: ['p2', 'content'],
+    endOffset: 9
+  });
+  var args = {selection: sel};
+  var out = copySelection(doc, args);
+  var copy = out.doc;
+  var img = copy.get('i1');
+  assert.isDefinedAndNotNull(img, 'The image should be copied.');
 });
