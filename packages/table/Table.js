dabed61f30feaf1123cf295e18ada320f59a3a13
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-01-18 17:07:58 +0100

    Introduced a test for paste transformation.
    Revealing a bug for pasting nested nodes.

--- packages/table/Table.js
+++ packages/table/Table.js
@@ -4,6 +4,7 @@ var BlockNode = require('../../model/BlockNode');
 var ParentNodeMixin = require('../../model/ParentNodeMixin');
 var TableMatrix = require('./TableMatrix');
 var TableCellIterator = require('./TableCellIterator');
+var uuid = require('../../util/uuid');
 
 function Table() {
   Table.super.apply(this, arguments);
@@ -43,7 +44,7 @@ BlockNode.extend(Table, ParentNodeMixin, function Table() {
     return new TableCellIterator(this);
   };
 
-  this.getSize = function ( dimension ) {
+  this.getSize = function(dimension) {
     var dim = this.matrix.getSize();
     if ( dimension === 'row' ) {
       return dim[0];
@@ -54,6 +55,22 @@ BlockNode.extend(Table, ParentNodeMixin, function Table() {
     }
   };
 
+  this.toTSV = function() {
+    var cell;
+    var data = [];
+    var row = [];
+    var it = this.getIterator();
+    it.onNewRow = function() {
+      row = [];
+      data.push(row);
+    };
+    while( (cell = it.next()) ) {
+      row.push(cell.content);
+    }
+    return data.map(function(row) {
+      return row.join('\t');
+    }).join('\n');
+  };
 });
 
 Table.static.name = "table";
@@ -61,5 +78,46 @@ Table.static.name = "table";
 Table.static.defineSchema({
   "sections": { type: ["id"], 'default': [] }
 });
+
+Table.fromTSV = function(tx, tsv, sep) {
+  sep = sep || '\t';
+  var lines = tsv.split(/\n/);
+  var tableId = uuid('table');
+  var sectionData = {
+    type: 'table-section',
+    id: tableId+'-body',
+    sectionType: 'tbody',
+    rows: [],
+    parent: tableId,
+  };
+  for (var i = 0; i < lines.length; i++) {
+    var rowData = {
+      type: 'table-row',
+      id: tableId + '-row'+i,
+      cells: [],
+      parent: sectionData.id,
+    };
+    var cells = lines[i].split(sep);
+    for (var j = 0; j < cells.length; j++) {
+      var cellStr = cells[j];
+      var cellData = {
+        type: 'table-cell',
+        id: rowData.id + '-col' + j,
+        cellType: 'td',
+        content: cellStr,
+        parent: rowData.id,
+      };
+      var cell = tx.create(cellData);
+      rowData.cells.push(cell.id);
+    }
+    var row = tx.create(rowData);
+    sectionData.rows.push(row.id);
+  }
+  return tx.create({
+    type: 'table',
+    id: tableId,
+    sections: [sectionData.id]
+  });
+};
 
 module.exports = Table;
