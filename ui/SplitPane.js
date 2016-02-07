3c20336938e28c0b8ae7a85cc2e553b91d6d5afa
Michael Aufreiter <michael@substance.io>
2016-02-07 22:08:14 +0100

    Change size on drag of split pane divider (WIP)

--- ui/SplitPane.js
+++ ui/SplitPane.js
@@ -2,6 +2,7 @@
 
 var Component = require('./Component');
 var $$ = Component.$$;
+var $ = require('../util/jquery');
 
 /**
   A split view layout component. Takes properties for configuration and 2 children via append.
@@ -42,19 +43,42 @@ SplitPane.Prototype = function() {
     }
   };
 
+  this.didMount = function() {
+    window.document.addEventListener('mouseup', this.onMouseUp.bind(this));
+    window.document.addEventListener('mousemove', this.onMouseMove.bind(this));
+  };
 
-  this.mouseDown = function(e) {
-    console.log('pos', e.clientX);
+  this.componentWillUnmount = function() {
+    window.document.removeEventListener('mouseup', this.onMouseUp.bind(this));
+    window.document.removeEventListener('mousemove', this.onMouseMove.bind(this));
+  };
+
+  this.unFocus = function() {
+    if (window.document.selection) {
+      window.document.selection.empty();
+    } else {
+      window.getSelection().removeAllRanges();
+    }
+  };
+
+  this.onMouseDown = function(e) {
+    this._pos = e.clientX;
+    this._resizing = true;
+    this.unFocus();
   };
 
   this.onMouseMove = function(e) {
-    console.log('pos', e.clientX);
+    if (this._resizing) {
+      this.unFocus();
+      this._pos = e.clientX;
+      // TODO: calculate pos relative to split pane not to window.
+      $(this.el.children[0]).css(this.getSizedStyle(this._pos));
+    }
   };
 
-  this.mouseUp = function() {
-    console.log('pos', e.clientX);
+  this.onMouseUp = function() {
+    this._resizing = false;
   };
-
 
   this.render = function() {
     if (this.props.children.length !== 2) {
@@ -71,21 +95,25 @@ SplitPane.Prototype = function() {
     var paneA = this.props.children[0];
     var paneB = this.props.children[1];
 
-    // Apply configured size either to pane A or B.
-    if (this.props.sizeB) {
-      paneB.addClass('se-pane sm-sized');
-      paneB.css(this.getSizedStyle(this.props.sizeB));
-      paneA.addClass('se-pane sm-auto-fill');
-    } else {
+    if (this._pos) {
       paneA.addClass('se-pane sm-sized');
-      paneA.css(this.getSizedStyle(this.props.sizeA));
+      paneA.css(this.getSizedStyle(this._pos));
       paneB.addClass('se-pane sm-auto-fill');
+    } else {
+      // Apply configured size either to pane A or B.
+      if (this.props.sizeB) {
+        paneB.addClass('se-pane sm-sized');
+        paneB.css(this.getSizedStyle(this.props.sizeB));
+        paneA.addClass('se-pane sm-auto-fill');
+      } else {
+        paneA.addClass('se-pane sm-sized');
+        paneA.css(this.getSizedStyle(this.props.sizeA));
+        paneB.addClass('se-pane sm-auto-fill');
+      }      
     }
 
-    var dividerEl = $$('div').addClass('se-split-pane-divider')
-                        .on('mousedown', this.mouseDown)
-                        .on('mousemove', this.mouseMove)
-                        .on('mouseup', this.mouseUp)
+    var dividerEl = $$('div').addClass('se-split-pane-divider').ref('divider')
+                        .on('mousedown', this.onMouseDown);
 
     el.append(
       paneA,
