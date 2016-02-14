58f024bcbf0829c12e1902852ef2429ee0435e4a
Michael Aufreiter <michael@substance.io>
2016-02-14 19:02:24 +0100

    More modular CollabSession.
    Does no longer require a websocket immediately. An explicit open call
    now connects the CollabSession to the remote endpoint.

--- util/WebSocket.js
+++ util/WebSocket.js
@@ -25,6 +25,24 @@ WebSocket.Prototype = function() {
     this.messageQueue.connectClientSocket(this);
   };
 
+  /*
+    Emulating native addEventListener API
+  */
+  this.addEventListener = function(eventName, handler) {
+    if (this['on'+eventName]) {
+      // For simplicity we only support a single handler per event atm
+      console.warn('on'+eventName, ' is already set. Overriding handler.');
+    } 
+    this['on'+eventName] = handler;
+  };
+
+  /*
+    Emulating native removeEventListener API
+  */
+  this.removeEventListener = function(eventName, handler) {
+    delete this['on'+eventName];
+  };
+
   /**
     Gets called by the message queue to handle a message
   */
