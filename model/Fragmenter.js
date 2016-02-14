66e975dc56ac9453479162832752f7458de2b6d9
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-02-14 22:36:58 +0100

    Fix another Fragmenter issue with having multiple collapsed fragments at one position.

--- model/Fragmenter.js
+++ model/Fragmenter.js
@@ -213,6 +213,18 @@ Fragmenter.Prototype = function() {
   function _compareClosers(a, b) {
     if (a.pos < b.pos) return -1;
     if (a.pos > b.pos) return 1;
+    // this makes closer be sorted in inverse order of openers
+    // to reduce stack sice
+    // HACK: a bit trial error. When we have to collapsed annotations
+    // at the same position then we want the closers in the same order
+    // as the openers.
+    if (a.pos === a.opener.pos && b.pos === b.opener.pos) {
+      if (a.opener.idx < b.opener.idx) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }
     if (a.opener.idx > b.opener.idx) return -1;
     if (a.opener.idx < b.opener.idx) return 1;
     return 0;
--- test/unit/model/Fragmenter.test.js
+++ test/unit/model/Fragmenter.test.js
@@ -171,9 +171,20 @@ QUnit.test("Two subsequent inline nodes.", function(assert) {
 
 QUnit.test("Collapsed annotation.", function(assert) {
   var annos = [
-    new Anno('a', 'inline1', 0, 0, {
-    }),
+    new Anno('a', 'a1', 0, 0, {
+    })
   ];
   var html = _render(TEXT, annos);
   assert.equal(html, '<a></a>ABCDEFGHI');
+});
+
+QUnit.test("Two collapsed annotations.", function(assert) {
+  var annos = [
+    new Anno('a', 'a1', 0, 0, {
+    }),
+    new Anno('b', 'b2', 0, 0, {
+    })
+  ];
+  var html = _render(TEXT, annos);
+  assert.equal(html, '<a></a><b></b>ABCDEFGHI');
 });
