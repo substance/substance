abe363797acc4c811a5d5d3ccc43acb5f7ccf919
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-02-14 20:03:30 +0100

    Added a test-case revealing the cause for #458.

--- test/unit/model/ContainerSelection.test.js
+++ test/unit/model/ContainerSelection.test.js
@@ -1,12 +1,37 @@
 'use strict';
 
 require('../qunit_extensions');
-var sample = require('../../fixtures/container_anno_sample');
+var containerAnnoSample = require('../../fixtures/container_anno_sample');
+var Selection = require('../../../model/Selection');
+var Range = require('../../../model/Range');
+var Coordinate = require('../../../model/Coordinate');
+var ContainerSelection = require('../../../model/ContainerSelection');
 
 QUnit.module('model/ContainerSelection');
 
+QUnit.test("Creating a ContainerSelection", function(assert) {
+  var sel = new ContainerSelection('main',['p1', 'content'], 1, ['p2', 'content'], 2);
+  assert.ok(sel.isContainerSelection(), 'Should be a container selection.');
+  assert.deepEqual(sel.startPath, ['p1', 'content'], 'startPath should be correct.');
+  assert.equal(sel.startOffset, 1, 'startOffset should be correct.');
+  assert.deepEqual(sel.endPath, ['p2', 'content'], 'endPath should be correct.');
+  assert.equal(sel.endOffset, 2, 'endOffset should be correct.');
+  assert.ok(!sel.isReverse(), 'Selection should not be reverse');
+});
+
+QUnit.test("Creating a ContainerSelection using a Range", function(assert) {
+  var range = new Range(new Coordinate(['p1', 'content'], 1), new Coordinate(['p2', 'content'], 2), false, 'main');
+  var sel = new Selection.create(range);
+  assert.ok(sel.isContainerSelection(), 'Should be a container selection.');
+  assert.deepEqual(sel.startPath, ['p1', 'content'], 'startPath should be correct.');
+  assert.equal(sel.startOffset, 1, 'startOffset should be correct.');
+  assert.deepEqual(sel.endPath, ['p2', 'content'], 'endPath should be correct.');
+  assert.equal(sel.endOffset, 2, 'endOffset should be correct.');
+  assert.ok(!sel.isReverse(), 'Selection should not be reverse');
+});
+
 QUnit.test("Expand with PropertySelection", function(assert) {
-  var doc = sample();
+  var doc = containerAnnoSample();
   var containerSel = doc.createSelection({
     type: 'container',
     containerId: 'main',
