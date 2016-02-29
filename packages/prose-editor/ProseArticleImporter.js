018f11f1c60a99c0bbf955f24ee2c9215075568a
Daniel Beilinson <beilinson@gmail.com>
2016-02-29 20:58:33 +0300

    Fixed convertDocument.

--- packages/prose-editor/ProseArticleImporter.js
+++ packages/prose-editor/ProseArticleImporter.js
@@ -27,8 +27,7 @@ HTMLImporter.extend(ProseArticleImporter, function() {
   /*
     Takes an HTML string.
   */
-  this.convertDocument = function(html) {
-    var bodyEls = DefaultDOMElement.parseHTML(html);
+  this.convertDocument = function(bodyEls) {
     // Just to make sure we always get an array of elements
     if (!bodyEls.length) bodyEls = [bodyEls];
     this.convertContainer(bodyEls, 'body');
