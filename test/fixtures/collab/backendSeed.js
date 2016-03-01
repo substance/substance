76afda89e21859345c71c47ca6cf3daf6513acdb
Michael Aufreiter <michael@substance.io>
2016-03-01 16:10:36 +0100

    Provide a backend seed.

---
+++ test/fixtures/collab/backendSeed.js
@@ -0,0 +1,18 @@
+var exampleNoteChangeset = require('./model/exampleNoteChangeset')();
+var twoParagraphs = require('./two-paragraphs');
+
+// Fixture for backend
+var testSeed = {
+  users: {
+    'test': {
+      userId: 'test',
+      name: 'Test user',
+      loginKey: '1234'      
+    }
+  },
+  changesets: {
+    'test-doc': twoParagraphs.createChangeset()
+  }
+};
+
+module.exports = testSeed;
