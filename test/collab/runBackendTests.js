62809bd4d3e76df37d579fc70c6f07ec9a23f461
Michael Aufreiter <michael@substance.io>
2016-03-06 18:05:50 +0100

    Group backend tests.

--- test/collab/runBackendTests.js
+++ test/collab/runBackendTests.js
@@ -17,29 +17,6 @@ function runBackendTests(backend, QUnit) {
     });
   });
 
-  QUnit.test("Test if seed db test-doc has the right version", function(assert) {
-    var done = assert.async();
-    backend.getVersion('test-doc', function(err, version) {
-      assert.notOk(err, 'Should not error');
-      assert.equal(version, 1, 'Document version should equals 1');
-      done();
-    });
-  });
-
-  QUnit.test("Test if seed db test-doc has valid changes", function(assert) {
-    var done = assert.async();
-    var args = {
-      documentId: 'test-doc',
-      sinceVersion: 0
-    };
-    backend.getChanges(args, function(err, result) {
-      assert.notOk(err, 'Should not error');
-      assert.equal(result.changes.length, 1, 'Should be only one change');
-      assert.equal(result.currentVersion, 1, 'Document version should equals 1');
-      done();
-    });
-  });
-
   QUnit.test('Create a new document', function(assert) {
     var done = assert.async();
     var args = {
@@ -75,43 +52,6 @@ function runBackendTests(backend, QUnit) {
     });
   });
 
-  QUnit.test('Should not allow adding a change to non existing changeset', function(assert) {
-    var done = assert.async();
-    var args = {
-      documentId: 'some-non-existent-doc',
-      change: {'some': 'change'},
-      userId: '1'
-    };
-    backend.addChange(args, function(err) {
-      assert.ok(err, 'Adding change to non existent doc should error');
-      done();
-    });
-  });
-
-  QUnit.test('Add a change to an existing doc', function(assert) {
-    var done = assert.async();
-    var args = {
-      documentId: 'test-doc',
-      change: {'some': 'change'},
-      userId: '1'
-    };
-    backend.addChange(args, function(err, version) {
-      assert.notOk(err, 'Should not error');
-      assert.equal(version, 2, 'Version should have been incremented by 1');
-
-      var args = {
-        documentId: 'test-doc',
-        sinceVersion: 0
-      };
-
-      backend.getChanges(args, function(err, result) {
-        assert.equal(result.changes.length, 2, 'There should be two changes in the db');
-        assert.equal(result.currentVersion, 2, 'New version should be 2');
-        done();
-      });
-    });
-  });
-
   QUnit.test('List documents', function(assert) {
     var done = assert.async();
     backend.listDocuments({}, function(err, documents) {
@@ -140,6 +80,68 @@ function runBackendTests(backend, QUnit) {
       assert.notOk(err, 'Should not error');
       assert.equal(documents.length, 0, 'There should be no matches');
       done();
+    });
+  });
+
+  // Changes Store API (Required by CollabEngine)
+  // --------------------
+
+  QUnit.test("Test if seed db test-doc has the right version", function(assert) {
+    var done = assert.async();
+    backend.getVersion('test-doc', function(err, version) {
+      assert.notOk(err, 'Should not error');
+      assert.equal(version, 1, 'Document version should equals 1');
+      done();
+    });
+  });
+
+  QUnit.test("Test if seed db test-doc has valid changes", function(assert) {
+    var done = assert.async();
+    var args = {
+      documentId: 'test-doc',
+      sinceVersion: 0
+    };
+    backend.getChanges(args, function(err, result) {
+      assert.notOk(err, 'Should not error');
+      assert.equal(result.changes.length, 1, 'Should be only one change');
+      assert.equal(result.currentVersion, 1, 'Document version should equals 1');
+      done();
+    });
+  });
+
+  QUnit.test('Should not allow adding a change to non existing changeset', function(assert) {
+    var done = assert.async();
+    var args = {
+      documentId: 'some-non-existent-doc',
+      change: {'some': 'change'},
+      userId: '1'
+    };
+    backend.addChange(args, function(err) {
+      assert.ok(err, 'Adding change to non existent doc should error');
+      done();
+    });
+  });
+
+  QUnit.test('Add a change to an existing doc', function(assert) {
+    var done = assert.async();
+    var args = {
+      documentId: 'test-doc',
+      change: {'some': 'change'},
+      userId: '1'
+    };
+    backend.addChange(args, function(err, version) {
+      assert.notOk(err, 'Should not error');
+      assert.equal(version, 2, 'Version should have been incremented by 1');
+      var args = {
+        documentId: 'test-doc',
+        sinceVersion: 0
+      };
+
+      backend.getChanges(args, function(err, result) {
+        assert.equal(result.changes.length, 2, 'There should be two changes in the db');
+        assert.equal(result.currentVersion, 2, 'New version should be 2');
+        done();
+      });
     });
   });
 
