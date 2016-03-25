edd1254188735ecabb9ee7be4b41bf8450195aef
Michael Aufreiter <michael@substance.io>
2016-03-25 22:44:05 +0100

    Comment out all collab server tests.

--- test/unit/model/CollabServer.test.js
+++ test/unit/model/CollabServer.test.js
@@ -1,143 +1,143 @@
-'use strict';
+// 'use strict';
 
-require('../qunit_extensions');
+// require('../qunit_extensions');
 
-var MessageQueue = require('../../collab/MessageQueue');
-var TestWebSocketServer = require('../../collab/TestWebSocketServer');
-var TestCollabSession = require('../../collab/TestCollabSession');
-var TestCollabServer = require('../../collab/TestCollabServer');
-var TestWebSocketConnection = require('../../collab/TestWebSocketConnection');
-var CollabClient = require('../../../collab/CollabClient');
-var DocumentStore = require('../../../collab/DocumentStore');
-var ChangeStore = require('../../../collab/ChangeStore');
-var DocumentEngine = require('../../../collab/DocumentEngine');
+// var MessageQueue = require('../../collab/MessageQueue');
+// var TestWebSocketServer = require('../../collab/TestWebSocketServer');
+// var TestCollabSession = require('../../collab/TestCollabSession');
+// var TestCollabServer = require('../../collab/TestCollabServer');
+// var TestWebSocketConnection = require('../../collab/TestWebSocketConnection');
+// var CollabClient = require('../../../collab/CollabClient');
+// var DocumentStore = require('../../../collab/DocumentStore');
+// var ChangeStore = require('../../../collab/ChangeStore');
+// var DocumentEngine = require('../../../collab/DocumentEngine');
 
-var documentStoreSeed = require('../../fixtures/collab/documentStoreSeed');
-var changeStoreSeed = require('../../fixtures/collab/changeStoreSeed');
-var twoParagraphs = require('../../fixtures/collab/two-paragraphs');
-var DocumentChange = require('../../../model/DocumentChange');
-var cloneDeep = require('lodash/cloneDeep');
+// var documentStoreSeed = require('../../fixtures/collab/documentStoreSeed');
+// var changeStoreSeed = require('../../fixtures/collab/changeStoreSeed');
+// var twoParagraphs = require('../../fixtures/collab/two-paragraphs');
+// var DocumentChange = require('../../../model/DocumentChange');
+// var cloneDeep = require('lodash/cloneDeep');
 
-QUnit.module('model/CollabServer');
+// QUnit.module('model/CollabServer');
 
-var doc1, doc2, session1, session2;
-var documentEngine, messageQueue, changeStore, documentStore;
-var wss, conn1,conn2;
-var hub, client1, client2;
+// var doc1, doc2, session1, session2;
+// var documentEngine, messageQueue, changeStore, documentStore;
+// var wss, conn1,conn2;
+// var hub, client1, client2;
 
-function _setup() {
-  doc1 = twoParagraphs.createArticle();
-  doc2 = twoParagraphs.createArticle();
+// function _setup() {
+//   doc1 = twoParagraphs.createArticle();
+//   doc2 = twoParagraphs.createArticle();
 
-  documentStore = new DocumentStore().seed(documentStoreSeed);
-  changeStore = new ChangeStore().seed(changeStoreSeed);
+//   documentStore = new DocumentStore().seed(documentStoreSeed);
+//   changeStore = new ChangeStore().seed(changeStoreSeed);
 
-  documentEngine = new DocumentEngine({
-    documentStore: documentStore,
-    changeStore: changeStore,
-    schemas: {
-      'prose-article': {
-        name: 'prose-article',
-        version: '1.0.0',
-        documentFactory: twoParagraphs
-      }
-    }
-  });
+//   documentEngine = new DocumentEngine({
+//     documentStore: documentStore,
+//     changeStore: changeStore,
+//     schemas: {
+//       'prose-article': {
+//         name: 'prose-article',
+//         version: '1.0.0',
+//         documentFactory: twoParagraphs
+//       }
+//     }
+//   });
 
-  messageQueue = new MessageQueue();
+//   messageQueue = new MessageQueue();
   
-  wss = new TestWebSocketServer({
-    messageQueue: messageQueue,
-    serverId: 'hub',
-    // manualConnect: true
-  });
+//   wss = new TestWebSocketServer({
+//     messageQueue: messageQueue,
+//     serverId: 'hub',
+//     // manualConnect: true
+//   });
 
-  hub = new TestCollabServer({
-    documentEngine: documentEngine
-  });
+//   hub = new TestCollabServer({
+//     documentEngine: documentEngine
+//   });
 
-  hub.bind(wss);
+//   hub.bind(wss);
 
-  // Once the server we can open connections
-  conn1 = new TestWebSocketConnection({
-    messageQueue: messageQueue,
-    clientId: 'user1',
-    serverId: 'hub',
-    // manualConnect: true
-  });
+//   // Once the server we can open connections
+//   conn1 = new TestWebSocketConnection({
+//     messageQueue: messageQueue,
+//     clientId: 'user1',
+//     serverId: 'hub',
+//     // manualConnect: true
+//   });
 
-  conn2 = new TestWebSocketConnection({
-    messageQueue: messageQueue,
-    clientId: 'user2',
-    serverId: 'hub',
-    // manualConnect: true
-  });
+//   conn2 = new TestWebSocketConnection({
+//     messageQueue: messageQueue,
+//     clientId: 'user2',
+//     serverId: 'hub',
+//     // manualConnect: true
+//   });
 
-  client1 = new CollabClient({
-    connection: conn1
-  });
-  client2 = new CollabClient({
-    connection: conn2
-  });
+//   client1 = new CollabClient({
+//     connection: conn1
+//   });
+//   client2 = new CollabClient({
+//     connection: conn2
+//   });
 
-  session1 = new TestCollabSession(doc1, {
-    collabClient: client1,
-    documentId: 'test-doc',
-    version: 1
-  });
-  session2 = new TestCollabSession(doc2, {
-    collabClient: client2,
-    documentId: 'test-doc',
-    version: 1
-  });
+//   session1 = new TestCollabSession(doc1, {
+//     collabClient: client1,
+//     documentId: 'test-doc',
+//     version: 1
+//   });
+//   session2 = new TestCollabSession(doc2, {
+//     collabClient: client2,
+//     documentId: 'test-doc',
+//     version: 1
+//   });
 
-  // HACK: overriding the CollabSession.startAutoCommit() to prevent
-  // the session from auto-committing and running forever
-  session1.startAutoCommit = function(){};
-  session2.startAutoCommit = function(){};
+//   // HACK: overriding the CollabSession.startAutoCommit() to prevent
+//   // the session from auto-committing and running forever
+//   session1.startAutoCommit = function(){};
+//   session2.startAutoCommit = function(){};
 
-  // this connects the peers 'physically'
-  // wss.connect();
-  // conn1.connect();
-  // conn2.connect();
+//   // this connects the peers 'physically'
+//   // wss.connect();
+//   // conn1.connect();
+//   // conn2.connect();
 
-  // HACK: to avoid that the clients send new messages
-  // we don't deliver the 'open' event
-  // Now we need to make sure that the hubClients are still handling messages afterwards
-  // client1._connect();
-  // client2._connect();
+//   // HACK: to avoid that the clients send new messages
+//   // we don't deliver the 'open' event
+//   // Now we need to make sure that the hubClients are still handling messages afterwards
+//   // client1._connect();
+//   // client2._connect();
 
-  // HACK: as we are just replaying messages here,
-  // we need to apply the local change, which would is usually done before the
-  // message is sent
-  messageQueue.on('message:sent', function(message) {
-    if (message.to === 'hub' && message.data.type === 'commit') {
-      var change = DocumentChange.fromJSON(message.data.change);
-      if (message.from === 'user1') {
-        doc1._apply(change);
-        session1._afterCommit(change);
-      } else if (message.from === 'user2') {
-        doc2._apply(change);
-        session2._afterCommit(change);
-      }
-    }
-  });
-}
+//   // HACK: as we are just replaying messages here,
+//   // we need to apply the local change, which would is usually done before the
+//   // message is sent
+//   messageQueue.on('message:sent', function(message) {
+//     if (message.to === 'hub' && message.data.type === 'commit') {
+//       var change = DocumentChange.fromJSON(message.data.change);
+//       if (message.from === 'user1') {
+//         doc1._apply(change);
+//         session1._afterCommit(change);
+//       } else if (message.from === 'user2') {
+//         doc2._apply(change);
+//         session2._afterCommit(change);
+//       }
+//     }
+//   });
+// }
 
-function _play(messages) {
-  messageQueue.clear();
+// function _play(messages) {
+//   messageQueue.clear();
 
-  messages.forEach(function(message) {
-    if (message.from === 'hub') {
-      // When we hit a server message we flush the queue, so the server
-      // can do the work
-      messageQueue.flush();
-    } else {
-      messageQueue.pushMessage(cloneDeep(message));
-    }
-  });
-  messageQueue.flush();
-}
+//   messages.forEach(function(message) {
+//     if (message.from === 'hub') {
+//       // When we hit a server message we flush the queue, so the server
+//       // can do the work
+//       messageQueue.flush();
+//     } else {
+//       messageQueue.pushMessage(cloneDeep(message));
+//     }
+//   });
+//   messageQueue.flush();
+// }
 
 // QUnit.test("Insert at same position", function(assert) {
 //   /*
