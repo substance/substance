968354ec0aff342073dbe5d054598618cafd4b57
Michael Aufreiter <michael@substance.io>
2016-04-11 10:46:12 +0200

    Default padding for layouts.

--- ui/Layout.js
+++ ui/Layout.js
@@ -1,6 +1,25 @@
 var Component = require('./Component');
 var $$ = Component.$$;
 
+/**
+  Simple layout component for simple layout tasks, without writing CSS.
+
+  @class
+  @component
+
+  @prop {String} width 'small', 'medium', 'large' and 'full'
+  @prop {String} [type] 'centered', 'left-aligned' or 'right-aligned'
+  @prop {String} [noPadding] No padding around layout, will fill the whole space available
+
+  @example
+
+  ```js
+  var form = $$(Layout, {
+    width: 'large',
+    type: 'centered'
+  });
+  ```
+*/
 function Layout() {
   Component.apply(this, arguments);
 }
@@ -12,6 +31,9 @@ Layout.Prototype = function() {
     el.addClass('sm-width-'+this.props.width);
     el.addClass('sm-type-'+this.props.type);
 
+    if (this.props.noPadding) {
+      el.addClass('sm-no-padding');
+    }
 
     el.append(this.props.children);
     return el;
