f7c160a02ee3d442f84d722525c02bc8e698cb2e
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-04-12 01:15:27 +0200

    Fixes and clean up.

--- ui/InlineNodeComponent.js
+++ ui/InlineNodeComponent.js
@@ -8,6 +8,18 @@ function InlineNodeComponent() {
 
 InlineNodeComponent.Prototype = function() {
 
+  var _super = InlineNodeComponent.super.prototype;
+
+  // TODO: we should avoid to have a didMount hook on an abstract base class
+  this.didMount = function() {
+    _super.didMount.apply(this, arguments);
+  };
+
+  // TODO: we should avoid to have a didMount hook on an abstract base class
+  this.dispose = function() {
+    _super.dispose.apply(this, arguments);
+  };
+
   this.render = function($$) {
     return $$('span')
       .attr({
