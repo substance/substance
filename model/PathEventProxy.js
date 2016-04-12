f3e33e12032ebac5f58165b7977e0965dbd8fd76
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-04-12 02:52:47 +0200

    Improved DocumentNode event API.

--- model/PathEventProxy.js
+++ model/PathEventProxy.js
@@ -2,6 +2,7 @@
 
 var oo = require('../util/oo');
 var each = require('lodash/each');
+var isEqual = require('lodash/isEqual');
 var TreeIndex = require('../util/TreeIndex');
 
 var PathEventProxy = function(doc) {
@@ -11,6 +12,27 @@ var PathEventProxy = function(doc) {
 };
 
 PathEventProxy.Prototype = function() {
+
+  this.on = function(path, method, context) {
+    this._add(context, path, method);
+  };
+
+  // proxy.off(this)
+  // proxy.off(this, path)
+  // proxy.off(this, path, this.onPropertyUpdate)
+  this.off = function(context, path, method) {
+    this._remove(context, path, method);
+  };
+
+  this.connect = function(listener, path, method) {
+    console.warn('DEPRECATED: use proxy.on(path, this.onPropertyChange, this) instead');
+    this.on(path, method, listener);
+  };
+
+  this.disconnect = function(listener) {
+    console.warn('DEPRECATED: use proxy.off(this) instead');
+    this._removeAll(listener);
+  };
 
   this.onDocumentChanged = function(change, info, doc) {
     // stop if no listeners registered
@@ -26,37 +48,29 @@ PathEventProxy.Prototype = function() {
     }.bind(this));
   };
 
-  this._add = function(path, listener, method) {
+  this._add = function(listener, path, method) {
     if (!method) {
       throw new Error('Invalid argument: expected function but got ' + method);
     }
-    var entry = { path: path, method: method, listener: listener };
+    var entry = { listener: listener, path: path, method: method };
     this.listeners.add(path, entry);
     this._list.push(entry);
   };
 
-  this.connect = function(listener, path, method) {
-    this._add(path, listener, method);
-  };
-
-  this.disconnect = function(listener) {
+  this._remove = function(listener, path, method) {
     for (var i = 0; i < this._list.length; i++) {
-      if (this._list[i].listener === listener) {
+      var item = this._list[i];
+      var match = (
+        (!path     || isEqual(item.path, path)) &&
+        (!listener || item.listener === listener) &&
+        (!method   || item.method !== method)
+      );
+      if (match) {
         var entry = this._list[i];
         this._list.splice(i, 1);
         this.listeners.remove(entry.path, entry);
       }
     }
-  };
-
-  this.add = function(path, listener, method) {
-    console.warn('DEPRECATED: use PathEventProxy#connect(this, path, method) instead.');
-    return this._add(path, listener, method);
-  };
-
-  this.remove = function(path, listener) {
-    console.warn('DEPRECATED: use PathEventProxy#disconnect(this) instead.');
-    this.disconnect(listener);
   };
 
 };
