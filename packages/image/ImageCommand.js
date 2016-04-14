a97f229907438dcbdf44e58d5255641fa672d5dd
Michael Aufreiter <michael@substance.io>
2016-04-14 12:32:54 +0200

    Get rid of jQuery for ImageCommand.

--- packages/image/ImageCommand.js
+++ packages/image/ImageCommand.js
@@ -2,7 +2,7 @@
 
 var uuid = require('../../util/uuid');
 var SurfaceCommand = require('../../ui/SurfaceCommand');
-var $ = require('../../util/jquery');
+var DefaultDOMElement = require('../../ui/DefaultDOMElement');
 
 var ImageCommand = SurfaceCommand.extend({
 
@@ -38,29 +38,34 @@ var ImageCommand = SurfaceCommand.extend({
     // Return if command is disabled
     if (state.disabled) return;
 
-    var $surface = surface.$el;
-    var $inputEl = $('<input type="file" id="file_input" style="opacity: 0;">');
-    $surface.append($inputEl);
-    $inputEl.click();
+    var surfaceEl = surface.el;
+    var inputEl = DefaultDOMElement.createElement('input').attr({
+      type: 'file',
+      id: 'file_input',
+      style: 'opacity: 0;'
+    });
 
-    $inputEl.on('change', function(/*e*/) {
-      var file = $inputEl[0].files[0];
+    surfaceEl.append(inputEl);
+    inputEl.click();
+
+    inputEl.on('change', function(/*e*/) {
+      // Pick the first file
+      // TODO: is there a way to use DefaultDOMElement API to access the files property?
+      var file = inputEl.el.files[0];
 
       // We no longer need the file input
-      $inputEl.remove();
+      inputEl.remove();
 
       controller.uploadFile(file, function(err, figureUrl) {
         // NOTE: we are providing a custom beforeState, to make sure
         // thate the correct initial selection is used.
         surface.transaction(function(tx, args) {
-
           var newImage = {
             id: uuid('image'),
             type: 'image',
             src: figureUrl,
             previewSrc: figureUrl
           };
-
           // Note: returning the result which will contain an updated selection
           return surface.insertNode(tx, { selection: args.selection, node: newImage });
         });
