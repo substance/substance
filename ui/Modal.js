fa125f4c0624f1b1114bcb0ab8884830b123a19c
Michael Aufreiter <michael@substance.io>
2016-04-17 23:51:12 +0200

    Improved Modal component.

--- ui/Modal.js
+++ ui/Modal.js
@@ -24,12 +24,26 @@ function Modal() {
 }
 
 Modal.Prototype = function() {
+  this._closeModal = function(e) {
+    var closeSurfaceClick = e.target.classList.contains('sc-modal');
+    if (closeSurfaceClick) {
+      this.send('closeModal');
+    }
+  };
 
   this.render = function($$) {
     var el = $$('div').addClass('sc-modal');
-    el.addClass('sm-width-'+this.props.width);
+    el.on('click', this._closeModal);
 
-    el.append(this.props.children);
+    if (this.props.width) {
+      el.addClass('sm-width-'+this.props.width);  
+    }
+
+    el.append(
+      $$('div').addClass('se-body').append(
+        this.props.children
+      )
+    );
     return el;
   };
 };
