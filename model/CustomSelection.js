ad4cc838b864133b026f6ac1d92596098dd6696c
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-05-30 01:21:03 +0200

    Use CustomSelection.

--- model/CustomSelection.js
+++ model/CustomSelection.js
@@ -5,9 +5,10 @@ var isEqual = require('lodash/isEqual');
 
 var Selection = require('./Selection');
 
-function CustomSelection(data, surfaceId) {
+function CustomSelection(type, data, surfaceId) {
   Selection.call(this);
 
+  this.type = type;
   this.data = data;
   this.surfaceId = surfaceId;
 }
@@ -23,13 +24,14 @@ CustomSelection.Prototype = function() {
   };
 
   this.getType = function() {
-    return 'custom';
+    return this.type;
   };
 
   this.toJSON = function() {
     return {
       type: this.getType(),
-      data: cloneDeep(this.data)
+      data: cloneDeep(this.data),
+      surfaceId: this.surfaceId
     };
   };
 
@@ -46,7 +48,7 @@ CustomSelection.Prototype = function() {
 Selection.extend(CustomSelection);
 
 CustomSelection.fromJSON = function(json) {
-  return new CustomSelection(json.data || {});
+  return new CustomSelection(json.type, json.data || {}, json.surfaceId);
 };
 
 module.exports = CustomSelection;
