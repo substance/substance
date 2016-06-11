3189d3554adb0360f29133e40ff35676f2e3da77
Michael Aufreiter <michael@substance.io>
2016-06-11 20:11:58 +0200

    Introduce Prompt.Link and Prompt.Input

--- ui/Prompt.js
+++ ui/Prompt.js
@@ -44,6 +44,35 @@ Action.Prototype = function() {
 
 Component.extend(Action);
 
+
+/*
+  Link (represented as an icon)
+*/
+function Link() {
+  Action.super.apply(this, arguments);
+}
+
+Link.Prototype = function() {
+
+  this.render = function($$) {
+    var iconEl = this.context.iconProvider.renderIcon($$, this.props.name);
+    var el = $$('a')
+      .attr({
+        href: this.props.href,
+        title: this.props.title,
+        target: 'blank'
+      })
+      .addClass('se-action').append(
+        iconEl,
+        '\uFEFF' // Zero-width char so line-height property has an effect
+      );
+    return el;
+  };
+};
+
+Component.extend(Link);
+
+
 /*
   Label for the prompt (not interactive)
 */
@@ -60,6 +89,45 @@ Label.Prototype = function() {
 };
 
 Component.extend(Label);
+
+
+/*
+  Takes a path to a string property and makes it editable
+*/
+function Input() {
+  Component.apply(this, arguments);
+}
+
+Input.Prototype = function() {
+  this._onChange = function() {
+    var documentSession = this.context.documentSession;
+    var path = this.props.path;
+    var newVal = this.el.val();
+
+    documentSession.transaction(function(tx) {
+      tx.set(path, newVal);
+    });
+  };
+
+  this.render = function($$) {
+    var documentSession = this.context.documentSession;
+    var doc = documentSession.getDocument();
+    var val = doc.get(this.props.path);
+
+    var el = $$('input')
+      .attr({
+        value: val,
+        type: this.props.type || 'text',
+        placeholder: this.props.placeholder
+      })
+      .on('change', this._onChange)
+      .addClass('se-input');
+
+    return el;
+  };
+};
+
+Component.extend(Input);
 
 /*
   Divider can be used to sepate prompt items
@@ -78,6 +146,8 @@ Component.extend(Separator);
 
 Prompt.Action = Action;
 Prompt.Label = Label;
+Prompt.Link = Link;
+Prompt.Input = Input;
 Prompt.Separator = Separator;
 
 module.exports = Prompt;
