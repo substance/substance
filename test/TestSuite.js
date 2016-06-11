0ac7d8ffb59f511c3020e649d61318774f3a302b
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-06-11 10:37:42 +0200

    Simple routing for TestSuite.

--- test/TestSuite.js
+++ test/TestSuite.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var isEqual = require('lodash/isEqual');
 var startsWith = require('lodash/startsWith');
 var Component = require('../ui/Component');
+var DefaultDOMElement = require('../ui/DefaultDOMElement');
 var TestItem = require('./TestItem');
+var Router = require('../ui/Router');
 
 function TestSuite() {
   TestSuite.super.apply(this, arguments);
@@ -19,19 +22,39 @@ function TestSuite() {
 TestSuite.Prototype = function() {
 
   this.didMount = function() {
-    this.props.harness.runAllTests();
+    var document = DefaultDOMElement.wrapNativeElement(window.document);
+    document.on('keypress', this.onKeypress, this);
+
+    this.router.on('route:changed', this.onRouteChange, this);
+    this.router.start();
+
+    this.runTests();
+  };
+
+  this.dispose = function() {
+    var document = DefaultDOMElement.wrapNativeElement(window.document);
+    document.off(this);
+
+    this.router.off(this);
+  };
+
+  this.getInitialState = function() {
+    this.router = new Router();
+    return this.router.readRoute();
   };
 
   this.render = function($$) {
     var el = $$('div').addClass('sc-test-suite');
 
+    var state = this.state;
+
     var toolbar = $$('div').addClass('se-toolbar');
     var moduleSelect = $$('select').ref('moduleNames');
     moduleSelect.append($$('option').attr('value', '').append('---   All   --'));
     this.moduleNames.forEach(function(moduleName) {
-      moduleSelect.append(
-        $$('option').attr('value', moduleName).append(moduleName)
-      );
+      var option = $$('option').attr('value', moduleName).append(moduleName);
+      if (moduleName === state.filter) option.attr('selected', true);
+      moduleSelect.append(option);
     });
     moduleSelect.on('change', this.onModuleSelect);
     toolbar.append(moduleSelect);
@@ -46,10 +69,16 @@ TestSuite.Prototype = function() {
     return el;
   };
 
-  this.onModuleSelect = function() {
-    var filter = this.refs.moduleNames.htmlProp('value');
+  this.didUpdate = function(oldProps, oldState) {
+    if (!isEqual(oldState, this.state)) {
+      this.runTests();
+    }
+  };
+
+  this.runTests = function() {
     var testItems = this.refs.tests.getChildren();
     var tests = [];
+    var filter = this.state.filter || '';
     testItems.forEach(function(testItem) {
       var t = testItem.props.test;
       if(startsWith(t.moduleName, filter)) {
@@ -60,6 +89,34 @@ TestSuite.Prototype = function() {
       }
     });
     this.props.harness.runTests(tests);
+  };
+
+  this.onModuleSelect = function() {
+    var filter = this.refs.moduleNames.htmlProp('value');
+    this.extendState({
+      filter: filter
+    });
+    this.updateRoute();
+  };
+
+  this.onKeypress = function(event) {
+    // console.log('####', event);
+    var handled = false;
+    if (event.key === 'r') {
+      this.runTests();
+      handled = true;
+    }
+    if (handled) {
+      event.preventDefault();
+    }
+  };
+
+  this.updateRoute = function() {
+    this.router.writeRoute(this.state);
+  };
+
+  this.onRouteChange = function(newState) {
+    this.setState(newState);
   };
 
 };
