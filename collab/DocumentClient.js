52854c5abdfc216c91c12bf006175a0ceb644316
Daniel Beilinson <beilinson@gmail.com>
2016-06-14 01:13:45 +0300

    Use request util.

--- collab/DocumentClient.js
+++ collab/DocumentClient.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var oo = require('../util/oo');
-var $ = require('../util/jquery');
+var request = require('../util/request');
 
 /*
   HTTP client for talking with DocumentServer
@@ -11,29 +11,6 @@ function DocumentClient(config) {
 }
 
 DocumentClient.Prototype = function() {
-
-  /*
-    A generic request method
-  */
-  this._request = function(method, url, data, cb) {
-    var ajaxOpts = {
-      type: method,
-      url: url,
-      contentType: "application/json; charset=UTF-8",
-      dataType: "json",
-      success: function(data) {
-        cb(null, data);
-      },
-      error: function(err) {
-        // console.error(err);
-        cb(new Error(err.responseJSON.errorMessage));
-      }
-    };
-    if (data) {
-      ajaxOpts.data = JSON.stringify(data);
-    }
-    $.ajax(ajaxOpts);
-  };
 
   /*
     Create a new document on the server
@@ -50,7 +27,7 @@ DocumentClient.Prototype = function() {
     });
   */
   this.createDocument = function(newDocument, cb) {
-    this._request('POST', this.config.httpUrl, newDocument, cb);
+    this.request('POST', this.config.httpUrl, newDocument, cb);
   };
 
   /*
@@ -64,7 +41,7 @@ DocumentClient.Prototype = function() {
   */
 
   this.getDocument = function(documentId, cb) {
-    this._request('GET', this.config.httpUrl+documentId, null, cb);
+    this.request('GET', this.config.httpUrl+documentId, null, cb);
   };
 
   /*
@@ -78,7 +55,7 @@ DocumentClient.Prototype = function() {
   */
 
   this.deleteDocument = function(documentId, cb) {
-    this._request('DELETE', this.config.httpUrl+documentId, null, cb);
+    this.request('DELETE', this.config.httpUrl+documentId, null, cb);
   };
 
 };
