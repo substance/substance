ad667766c23e8d0ef58a3f342baef0eb42d5138a
Daniel Beilinson <beilinson@gmail.com>
2016-06-14 01:14:27 +0300

    Get rid of jQuery.

--- util/jquery.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var inBrowser = require('./inBrowser');
-
-if (inBrowser) {
-  module.exports = require('jquery');
-} else {
-  module.exports = require('./cheerio.customized.js');
-}
