c854d057de0e52b9bec2993da73c34fb79b2093f
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-06-15 14:46:02 +0200

    Render diff after equals in test results.

--- test/ResultItem.js
+++ test/ResultItem.js
@@ -16,14 +16,30 @@ ResultItem.Prototype = function() {
     var test = this.props.test;
     var result = this.props.result;
     var el = $$('div').addClass('sc-test-result');
+
+    var header = $$('div');
     if (!test._skip) {
       if (result.ok) {
-        el.append($$('span').addClass('se-status sm-ok').append("\u2713"));
+        header.append($$('span').addClass('se-status sm-ok').append("\u2713"));
       } else {
-        el.append($$('span').addClass('se-status sm-not-ok').append("\u26A0"));
+        header.append($$('span').addClass('se-status sm-not-ok').append("\u26A0"));
       }
     }
-    el.append($$('span').addClass('se-description').append(result.name));
+    header.append($$('span').addClass('se-description').append(result.name));
+    el.append(header);
+
+    if (!test._skip && !result.ok && result.operator === "equal") {
+      var diff = $$('div').addClass('se-diff');
+      var expected = $$('div').addClass('se-expected')
+        .append('Expected:')
+        .append($$('pre').append(String(result.expected)));
+      var actual = $$('div').addClass('se-actual')
+        .append('Actual:')
+        .append($$('pre').append(String(result.actual)));
+      diff.append(expected, actual);
+      el.append(diff);
+    }
+
     return el;
   };
 
