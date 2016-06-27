ee00d146e9b1b22dd3b7307aec04cc8da189e8ab
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-06-27 17:26:14 +0200

    Reduced code redundancy.

---
+++ ui/AbstractEditor.js
@@ -0,0 +1,118 @@
+'use strict';
+
+var Component = require('./Component');
+var CommandManager = require('./CommandManager');
+var SurfaceManager = require('./SurfaceManager');
+var MacroManager = require('./MacroManager');
+var GlobalEventHandler = require('./GlobalEventHandler');
+
+function AbstractEditor() {
+  AbstractEditor.super.apply(this, arguments);
+
+  this._initialize(this.props);
+}
+
+AbstractEditor.Prototype = function() {
+
+  this.didMount = function() {
+    this.documentSession.on('didUpdate', this._documentSessionUpdated, this);
+  };
+
+  this.willReceiveProps = function(nextProps) {
+    var newSession = nextProps.documentSession;
+    var shouldDispose = newSession && newSession !== this.documentSession;
+    if (shouldDispose) {
+      this._dispose();
+      this._initialize(nextProps);
+    }
+  };
+
+  this.dispose = function() {
+    this._dispose();
+  };
+
+  this._dispose = function() {
+    this.surfaceManager.dispose();
+    this.commandManager.dispose();
+    this.globalEventHandler.dispose();
+    this.documentSession.off(this);
+    // Note: we need to clear everything, as the childContext
+    // changes which is immutable
+    this.empty();
+  };
+
+  this.getChildContext = function() {
+    return {
+      controller: this,
+      iconProvider: this.iconProvider,
+      documentSession: this.documentSession,
+      doc: this.doc, // TODO: remove in favor of documentSession
+      componentRegistry: this.componentRegistry,
+      surfaceManager: this.surfaceManager,
+      commandManager: this.commandManager,
+      toolRegistry: this.toolRegistry,
+      labelProvider: this.labelProvider,
+      converterRegistry: this.converterRegistry,
+      globalEventHandler: this.globalEventHandler,
+      editingBehavior: this.editingBehavior
+    };
+  };
+
+  this._initialize = function(props) {
+    var configurator = props.configurator;
+    var commands = configurator.getCommands();
+    if (!props.documentSession) {
+      throw new Error('DocumentSession instance required');
+    }
+    this.documentSession = props.documentSession;
+    this.doc = this.documentSession.getDocument();
+    this.saveHandler = configurator.getSaveHandler();
+    this.documentSession.setSaveHandler(this.saveHandler);
+    this.componentRegistry = configurator.getComponentRegistry();
+    this.toolRegistry = configurator.getToolRegistry();
+    this.surfaceManager = new SurfaceManager(this.documentSession);
+    this.fileClient = configurator.getFileClient();
+    this.commandManager = new CommandManager(this.getCommandContext(), commands);
+    this.macroManager = new MacroManager(this.getMacroContext(), configurator.getMacros());
+    this.iconProvider = configurator.getIconProvider();
+    this.converterRegistry = configurator.getConverterRegistry();
+    this.globalEventHandler = new GlobalEventHandler(this.documentSession, this.surfaceManager);
+    this.editingBehavior = configurator.getEditingBehavior();
+    this.labelProvider = configurator.getLabelProvider();
+  };
+
+  this.getCommandContext = function() {
+    return {
+      documentSession: this.documentSession,
+      surfaceManager: this.surfaceManager,
+      fileClient: this.fileClient,
+      saveHandler: this.saveHandler,
+      converterRegistry: this.converterRegistry
+    };
+  };
+
+  this.getMacroContext = function() {
+    return {
+      documentSession: this.documentSession,
+      surfaceManager: this.surfaceManager
+    };
+  };
+
+  this.getToolbar = function() {
+    throw new Error('This method is abstract.');
+  };
+
+  this._documentSessionUpdated = function() {
+    var toolbar = this.getToolbar();
+    if (toolbar) {
+      var commandStates = this.commandManager.getCommandStates();
+      this.refs.toolbar.setProps({
+        commandStates: commandStates
+      });
+    }
+  };
+};
+
+Component.extend(AbstractEditor);
+
+module.exports = AbstractEditor;
---
+++ ui/DefaultOverlay.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Component = require('./Component');
+
+function DefaultOverlay() {
+  Component.apply(this, arguments);
+}
+
+DefaultOverlay.Prototype = function() {
+
+  this.render = function($$) {
+    var el = $$('div').addClass(this.getClassNames());
+    var commandStates = this.props.commandStates;
+    var toolRegistry = this.context.toolRegistry;
+    toolRegistry.forEach(function(tool) {
+      if (tool.options.overlay) {
+        var toolProps = tool.Class.static.getProps(commandStates);
+        if (toolProps) {
+          el.append(
+            $$(tool.Class, toolProps)
+          );
+        }
+      }
+    });
+    return el;
+  };
+
+  this.getClassNames = function() {
+    return "sc-overlay";
+  };
+
+};
+
+Component.extend(DefaultOverlay);
+
+module.exports = DefaultOverlay;
