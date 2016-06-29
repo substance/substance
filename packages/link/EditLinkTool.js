ef37324f4e1f3b1cbd4ca08a35db219308ad25d4
Michael Aufreiter <michael@substance.io>
2016-06-29 16:42:54 +0200

    Generalize EditLinkTool for reusability.

--- packages/link/EditLinkTool.js
+++ packages/link/EditLinkTool.js
@@ -16,21 +16,28 @@ function EditLinkTool() {
 
 EditLinkTool.Prototype = function() {
 
+  this.getUrlPath = function() {
+    var propPath = this.constructor.static.urlPropertyPath;
+    return [this.props.node.id].concat(propPath);
+  };
+
   this.render = function($$) {
     var node = this.props.node;
+    var doc = node.getDocument();
     var el = $$('div').addClass('sc-edit-link-tool');
+    var urlPath = this.getUrlPath();
 
     el.append(
       $$(Prompt).append(
         $$(Prompt.Input, {
           type: 'url',
-          path: [node.id, 'url'],
+          path: urlPath,
           placeholder: 'Paste or type a link url'
         }),
         $$(Prompt.Separator),
         $$(Prompt.Link, {
           name: 'open-link',
-          href: node.url,
+          href: doc.get(urlPath),
           title: this.getLabel('open-link')
         }),
         $$(Prompt.Action, {name: 'delete', title: this.getLabel('delete')})
@@ -43,14 +50,19 @@ EditLinkTool.Prototype = function() {
   this.onDelete = function(e) {
     e.preventDefault();
     var node = this.props.node;
-    var documentSession = this.context.documentSession;
-    documentSession.transaction(function(tx) {
+    var sm = this.context.surfaceManager;
+    var surface = sm.getFocusedSurface();
+    surface.transaction(function(tx, args) {
       tx.delete(node.id);
+      return args;
     });
   };
 };
 
 Component.extend(EditLinkTool);
+
+EditLinkTool.static.urlPropertyPath = ['url'];
+EditLinkTool.static.name = 'edit-link';
 
 EditLinkTool.static.getProps = function(commandStates) {
   if (commandStates.link.mode === 'edit') {
@@ -59,7 +71,5 @@ EditLinkTool.static.getProps = function(commandStates) {
     return undefined;
   }
 };
-
-EditLinkTool.static.name = 'edit-link';
 
 module.exports = EditLinkTool;
