d3a3782ee52e639fd1000b25b17dce0b0e35343e
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-07-06 09:32:03 +0200

    Cleanup.

--- packages/image/DropImage.js
+++ packages/image/DropImage.js
@@ -11,11 +11,10 @@ DropImage.Prototype = function() {
   this.drop = function(props, context) {
     // precondition: we need a surface and a selection
     // and act only if there are image files
-    var documentSession = context.documentSession;
     var surface = props.surface;
     var selection = props.selection;
     var files = props.files;
-    if (!documentSession || !surface || !selection || !files || files.length === 0) return;
+    if (!surface || !selection || !files || files.length === 0) return;
     // pick only the images
     files = files.filter(function(file) {
       return startsWith(file.type, 'image');
