f68efb685d8a392116b03aeb2385e7dc87c19ea1
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-07-15 15:18:36 +0200

    Make util/inBrowser electron compatible.

--- util/inBrowser.js
+++ util/inBrowser.js
@@ -2,6 +2,6 @@
 
 // Note: in iron-node window is defined - but it has window.process
 // which is not there in a real browser env
-var inBrowser = ( typeof window !== 'undefined' && !window.process );
+var inBrowser = ( typeof window !== 'undefined');
 
 module.exports = inBrowser;
