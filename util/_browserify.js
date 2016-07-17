f0af74a7becfeeee9da84b39532a84525a23aef6
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-07-17 06:47:34 +0200

    browserify option for full es6 support.

--- util/_browserify.js
+++ util/_browserify.js
@@ -26,6 +26,7 @@ function _browserify(params, cb) {
   var useBabelify = params.jsx || params.es6;
   if (useBabelify) {
     var plugins = [];
+    var presets = [];
     if (params.jsx) {
       plugins.push("syntax-jsx");
       plugins.push(
@@ -36,7 +37,34 @@ function _browserify(params, cb) {
         }]
       );
     }
-    if (params.es6) {
+    if (params.es6 === true || params.es6 === "full") {
+      plugins = plugins.concat([
+        "check-es2015-constants",
+        "transform-es2015-arrow-functions",
+        "transform-es2015-block-scoped-functions",
+        "transform-es2015-block-scoping",
+        "transform-es2015-classes",
+        "transform-es2015-computed-properties",
+        "transform-es2015-destructuring",
+        "transform-es2015-duplicate-keys",
+        "transform-es2015-for-of",
+        "transform-es2015-function-name",
+        "transform-es2015-literals",
+        ["transform-es2015-modules-commonjs-simple", {
+          "noMangle": true,
+          "addExports": true
+        }],
+        "transform-es2015-object-super",
+        "transform-es2015-parameters",
+        "transform-es2015-shorthand-properties",
+        "transform-es2015-spread",
+        "transform-es2015-sticky-regex",
+        "transform-es2015-template-literals",
+        "transform-es2015-typeof-symbol",
+        "transform-es2015-unicode-regex",
+        "transform-regenerator"
+      ]);
+    } else if (params.es6 === "modules") {
       plugins.push(
         // support for es6 import/export
         // Note: the rest of es6 is supported natively
@@ -50,6 +78,7 @@ function _browserify(params, cb) {
       );
     }
     b = b.transform("babelify", {
+      presets: presets,
       plugins: plugins
     });
   }
