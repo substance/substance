54b19c61c33639b3437e4bef486c2bdef10b41ff
Daniel Beilinson <beilinson@gmail.com>
2016-09-19 00:11:42 +0300

    ES6 AbstractEditor.

--- ui/AbstractEditor.js
+++ ui/AbstractEditor.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import Component from './Component'
 import CommandManager from './CommandManager'
 import SurfaceManager from './SurfaceManager'
@@ -7,42 +5,42 @@ import MacroManager from './MacroManager'
 import GlobalEventHandler from './GlobalEventHandler'
 import DragManager from './DragManager'
 
-function AbstractEditor() {
-  AbstractEditor.super.apply(this, arguments);
-  this._initialize(this.props);
-}
+class AbstractEditor extends Component {
+  
+  constructor(...args) {
+    super(...args)
+    this._initialize(this.props)
+  }
 
-AbstractEditor.Prototype = function() {
+  didMount() {
+    this.documentSession.on('didUpdate', this._documentSessionUpdated, this)
+  }
 
-  this.didMount = function() {
-    this.documentSession.on('didUpdate', this._documentSessionUpdated, this);
-  };
-
-  this.willReceiveProps = function(nextProps) {
-    var newSession = nextProps.documentSession;
-    var shouldDispose = newSession && newSession !== this.documentSession;
+  willReceiveProps(nextProps) {
+    let newSession = nextProps.documentSession
+    let shouldDispose = newSession && newSession !== this.documentSession
     if (shouldDispose) {
-      this._dispose();
-      this._initialize(nextProps);
+      this._dispose()
+      this._initialize(nextProps)
     }
-  };
+  }
 
-  this.dispose = function() {
-    this._dispose();
-  };
+  dispose() {
+    this._dispose()
+  }
 
-  this._dispose = function() {
-    this.surfaceManager.dispose();
-    this.commandManager.dispose();
-    this.globalEventHandler.dispose();
-    this.dragManager.dispose();
-    this.documentSession.off(this);
+  _dispose() {
+    this.surfaceManager.dispose()
+    this.commandManager.dispose()
+    this.globalEventHandler.dispose()
+    this.dragManager.dispose()
+    this.documentSession.off(this)
     // Note: we need to clear everything, as the childContext
     // changes which is immutable
-    this.empty();
-  };
+    this.empty()
+  }
 
-  this.getChildContext = function() {
+  getChildContext() {
     return {
       controller: this,
       iconProvider: this.iconProvider,
@@ -57,69 +55,67 @@ AbstractEditor.Prototype = function() {
       globalEventHandler: this.globalEventHandler,
       editingBehavior: this.editingBehavior,
       dragManager: this.dragManager,
-    };
-  };
+    }
+  }
 
-  this._initialize = function(props) {
-    var configurator = props.configurator;
-    var commands = configurator.getCommands();
+  _initialize(props) {
+    let configurator = props.configurator
+    let commands = configurator.getCommands()
     if (!props.documentSession) {
       throw new Error('DocumentSession instance required');
     }
-    this.documentSession = props.documentSession;
-    this.doc = this.documentSession.getDocument();
-    this.saveHandler = configurator.getSaveHandler();
-    this.documentSession.setSaveHandler(this.saveHandler);
-    this.componentRegistry = configurator.getComponentRegistry();
-    this.tools = configurator.getTools();
-    this.surfaceManager = new SurfaceManager(this.documentSession);
-    this.fileClient = configurator.getFileClient();
-    this.commandManager = new CommandManager(this.getCommandContext(), commands);
+    this.documentSession = props.documentSession
+    this.doc = this.documentSession.getDocument()
+    this.saveHandler = configurator.getSaveHandler()
+    this.documentSession.setSaveHandler(this.saveHandler)
+    this.componentRegistry = configurator.getComponentRegistry()
+    this.tools = configurator.getTools()
+    this.surfaceManager = new SurfaceManager(this.documentSession)
+    this.fileClient = configurator.getFileClient()
+    this.commandManager = new CommandManager(this.getCommandContext(), commands)
     this.dragManager = new DragManager(configurator.createDragHandlers(), {
       documentSession: this.documentSession,
       surfaceManager: this.surfaceManager,
       commandManager: this.commandManager,
     });
-    this.macroManager = new MacroManager(this.getMacroContext(), configurator.getMacros());
-    this.iconProvider = configurator.getIconProvider();
-    this.converterRegistry = configurator.getConverterRegistry();
-    this.globalEventHandler = new GlobalEventHandler(this.documentSession, this.surfaceManager);
-    this.editingBehavior = configurator.getEditingBehavior();
-    this.labelProvider = configurator.getLabelProvider();
-  };
+    this.macroManager = new MacroManager(this.getMacroContext(), configurator.getMacros())
+    this.iconProvider = configurator.getIconProvider()
+    this.converterRegistry = configurator.getConverterRegistry()
+    this.globalEventHandler = new GlobalEventHandler(this.documentSession, this.surfaceManager)
+    this.editingBehavior = configurator.getEditingBehavior()
+    this.labelProvider = configurator.getLabelProvider()
+  }
 
-  this.getCommandContext = function() {
+  getCommandContext() {
     return {
       documentSession: this.documentSession,
       surfaceManager: this.surfaceManager,
       fileClient: this.fileClient,
       saveHandler: this.saveHandler,
       converterRegistry: this.converterRegistry
-    };
-  };
+    }
+  }
 
-  this.getMacroContext = function() {
+  getMacroContext() {
     return {
       documentSession: this.documentSession,
       surfaceManager: this.surfaceManager
-    };
-  };
+    }
+  }
 
-  this.getToolbar = function() {
-    throw new Error('This method is abstract.');
-  };
+  getToolbar() {
+    throw new Error('This method is abstract.')
+  }
 
-  this._documentSessionUpdated = function() {
-    var toolbar = this.getToolbar();
+  _documentSessionUpdated() {
+    let toolbar = this.getToolbar()
     if (toolbar) {
-      var commandStates = this.commandManager.getCommandStates();
+      let commandStates = this.commandManager.getCommandStates()
       toolbar.setProps({
         commandStates: commandStates
-      });
+      })
     }
-  };
-};
+  }
+}
 
-Component.extend(AbstractEditor);
-
-export default AbstractEditor;
+export default AbstractEditor
