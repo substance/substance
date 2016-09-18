649fc3f93a3b4d6129c8be7350e78f7cf3262239
Daniel Beilinson <beilinson@gmail.com>
2016-09-19 00:11:34 +0300

    ES6 AnnotatedTextComponent.

--- ui/AnnotatedTextComponent.js
+++ ui/AnnotatedTextComponent.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import Fragmenter from '../model/Fragmenter'
 import Component from './Component'
 import AnnotationComponent from './AnnotationComponent'
@@ -15,11 +13,7 @@ import InlineNodeComponent from './InlineNodeComponent'
   @prop {String[]} path The property to be rendered.
 */
 
-function AnnotatedTextComponent() {
-  AnnotatedTextComponent.super.apply(this, arguments);
-}
-
-AnnotatedTextComponent.Prototype = function() {
+class AnnotatedTextComponent extends Component {
 
   // TODO: this component should listen on changes to the property
   // Otherwise will not be updated.
@@ -30,50 +24,51 @@ AnnotatedTextComponent.Prototype = function() {
 
     @return {VirtualNode} VirtualNode created using ui/Component
    */
-  this.render = function($$) {
-    var el = this._renderContent($$)
+  render($$) {
+    let el = this._renderContent($$)
       .addClass('sc-annotated-text')
       .css({
         whiteSpace: "pre-wrap"
-      });
-    return el;
-  };
+      })
 
-  this.getText = function() {
-    return this.getDocument().get(this.props.path) || '';
-  };
+    return el
+  }
 
-  this.getAnnotations = function() {
-    return this.getDocument().getIndex('annotations').get(this.props.path);
-  };
+  getText() {
+    return this.getDocument().get(this.props.path) || ''
+  }
 
-  this._renderContent = function($$) {
-    var text = this.getText();
-    var annotations = this.getAnnotations();
-    var el = $$(this.props.tagName || 'span');
+  getAnnotations() {
+    return this.getDocument().getIndex('annotations').get(this.props.path)
+  }
+
+  _renderContent($$) {
+    let text = this.getText();
+    let annotations = this.getAnnotations()
+    let el = $$(this.props.tagName || 'span')
     if (annotations && annotations.length > 0) {
-      var fragmenter = new Fragmenter({
+      let fragmenter = new Fragmenter({
         onText: this._renderTextNode.bind(this),
         onEnter: this._renderFragment.bind(this, $$),
         onExit: this._finishFragment.bind(this)
       });
-      fragmenter.start(el, text, annotations);
+      fragmenter.start(el, text, annotations)
     } else {
-      el.append(text);
+      el.append(text)
     }
-    return el;
-  };
+    return el
+  }
 
-  this._renderTextNode = function(context, text) {
+  _renderTextNode(context, text) {
     if (text && text.length > 0) {
-      context.append(text);
+      context.append(text)
     }
-  };
+  }
 
-  this._renderFragment = function($$, fragment) {
-    var doc = this.getDocument();
-    var componentRegistry = this.getComponentRegistry();
-    var node = fragment.node;
+  _renderFragment($$, fragment) {
+    let doc = this.getDocument()
+    let componentRegistry = this.getComponentRegistry()
+    let node = fragment.node
     if (node.type === "container-annotation-fragment") {
       return $$(AnnotationComponent, { doc: doc, node: node })
         .addClass("se-annotation-fragment")
@@ -82,39 +77,37 @@ AnnotatedTextComponent.Prototype = function() {
       return $$(AnnotationComponent, { doc: doc, node: node })
         .addClass("se-anchor")
         .addClass(node.anno.getTypeNames().join(' ').replace(/_/g, "-"))
-        .addClass(node.isStart?"start-anchor":"end-anchor");
+        .addClass(node.isStart?"start-anchor":"end-anchor")
     }
-    var ComponentClass = componentRegistry.get(node.type) || AnnotationComponent;
+    let ComponentClass = componentRegistry.get(node.type) || AnnotationComponent
     if (node.constructor.isInline &&
         // opt-out for custom implementations
         !ComponentClass.isCustom &&
         // also no extra wrapping if the node is already an inline node
         !ComponentClass.prototype._isInlineNodeComponent) {
-      ComponentClass = InlineNodeComponent;
+      ComponentClass = InlineNodeComponent
     }
-    var el = $$(ComponentClass, { doc: doc, node: node });
-    return el;
-  };
+    let el = $$(ComponentClass, { doc: doc, node: node })
+    return el
+  }
 
-  this._finishFragment = function(fragment, context, parentContext) {
-    parentContext.append(context);
-  };
+  _finishFragment(fragment, context, parentContext) {
+    parentContext.append(context)
+  }
 
   /**
     Gets document instance.
 
     @return {Document} The document instance
    */
-  this.getDocument = function() {
-    return this.props.doc || this.context.doc;
-  };
+  getDocument() {
+    return this.props.doc || this.context.doc
+  }
 
-  this.getComponentRegistry = function() {
-    return this.props.componentRegistry || this.context.componentRegistry;
-  };
+  getComponentRegistry() {
+    return this.props.componentRegistry || this.context.componentRegistry
+  }
 
-};
+}
 
-Component.extend(AnnotatedTextComponent);
-
-export default AnnotatedTextComponent;
+export default AnnotatedTextComponent
