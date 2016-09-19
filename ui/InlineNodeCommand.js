208361d417afb1f1d6efeedd9406e15371a4aada
Daniel Beilinson <beilinson@gmail.com>
2016-09-20 00:52:24 +0300

    ES6 command.

--- ui/InlineNodeCommand.js
+++ ui/InlineNodeCommand.js
@@ -1,85 +1,80 @@
-'use strict';
-
 import Command from './Command'
 import insertInlineNode from '../model/transform/insertInlineNode'
 
-function InlineNodeCommand() {
-  InlineNodeCommand.super.apply(this, arguments);
+class InlineNodeCommand extends Command {
+  constructor(...args) {
+    super(...args)
 
-  this.nodeType = this.params.nodeType;
+    this.nodeType = this.params.nodeType
 
-  if (!this.params.nodeType) {
-    throw new Error("Every AnnotationCommand must have an 'nodeType'");
+    if (!this.params.nodeType) {
+      throw new Error("Every AnnotationCommand must have an 'nodeType'")
+    }
   }
-}
-
-InlineNodeCommand.Prototype = function() {
 
   /**
     Get the type of an annotation.
 
     @returns {String} The annotation's type.
    */
-  this.getNodeType = function() {
-    return this.nodeType;
-  };
+  getNodeType() {
+    return this.nodeType
+  }
 
   // legacy
-  this.getAnnotationType = function() {
-    console.warn('DEPRECATED: ust InlineNodeCommand.getNodeType() instead.');
-    return this.getNodeType();
-  };
+  getAnnotationType() {
+    console.warn('DEPRECATED: ust InlineNodeCommand.getNodeType() instead.')
+    return this.getNodeType()
+  }
 
-  this.getCommandState = function(props, context) {
-    var sel = context.documentSession.getSelection();
-    var newState = {
+  getCommandState(props, context) {
+    let sel = context.documentSession.getSelection()
+    let newState = {
       disabled: true,
       active: false,
       node: undefined
-    };
+    }
 
     if (sel && !sel.isNull() && sel.isPropertySelection()) {
-      newState.disabled = false;
+      newState.disabled = false
     }
 
-    var annos = this._getAnnotationsForSelection(props, context);
+    let annos = this._getAnnotationsForSelection(props, context)
     if (annos.length === 1 && annos[0].getSelection().equals(sel)) {
-      newState.active = true;
-      newState.node = annos[0];
+      newState.active = true
+      newState.node = annos[0]
     }
 
-    return newState;
-  };
+    return newState
+  }
 
-  this._getAnnotationsForSelection = function(props) {
-    return props.selectionState.getAnnotationsForType(this.getNodeType());
-  };
+  _getAnnotationsForSelection(props) {
+    return props.selectionState.getAnnotationsForType(this.getNodeType())
+  }
 
-  this.execute = function(props, context) {
-    var state = this.getCommandState(props, context);
-    if (state.disabled) return;
-    var surface = context.surface ||context.surfaceManager.getFocusedSurface();
+  execute(props, context) {
+    let state = this.getCommandState(props, context)
+    if (state.disabled) return
+    let surface = context.surface ||context.surfaceManager.getFocusedSurface()
     if (surface) {
       surface.transaction(function(tx, args) {
-        return this.insertInlineNode(tx, args);
-      }.bind(this));
+        return this.insertInlineNode(tx, args)
+      }.bind(this))
     }
-    return true;
-  };
+    return true
+  }
 
-  this.insertInlineNode = function(tx, args) {
-    args.node = this.createNodeData(tx, args);
-    return insertInlineNode(tx, args);
-  };
+  insertInlineNode(tx, args) {
+    args.node = this.createNodeData(tx, args)
+    return insertInlineNode(tx, args)
+  }
 
-  this.createNodeData = function(tx, args) { // eslint-disable-line
+  createNodeData(tx, args) { // eslint-disable-line
     return {
       type: this.constructor.type
-    };
-  };
+    }
+  }
 
-};
+}
 
-Command.extend(InlineNodeCommand);
-
-export default InlineNodeCommand;
+export default InlineNodeCommand
