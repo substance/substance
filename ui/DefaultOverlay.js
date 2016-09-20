a9f643cb70af4bc90519e2a8842179d604812bc4
Michael Aufreiter <michael@substance.io>
2016-09-20 17:38:53 +0200

    Generalize DefaultOverlay.

--- ui/DefaultOverlay.js
+++ ui/DefaultOverlay.js
@@ -15,9 +15,13 @@ class DefaultOverlay extends Component {
     let tools = this.context.tools
     let overlayTools = tools.get('overlay')
 
-    overlayTools.forEach(function(tool) {
-      let toolProps = tool.Class.getProps(commandStates)
-      if (toolProps) {
+    overlayTools.forEach(function(tool, name) {
+      var toolProps = Object.assign({}, commandStates[name], {
+        name: name,
+        icon: name
+      })
+
+      if (toolProps && !toolProps.disabled) {
         el.append(
           $$(tool.Class, toolProps).ref(tool.name)
         );
