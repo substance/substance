cba8baa4df3251343eba48deb2f2de18fe47f6d6
Daniel Beilinson <beilinson@gmail.com>
2016-09-21 13:49:56 +0300

    ES6 ComponentRegistry.

--- ui/ComponentRegistry.js
+++ ui/ComponentRegistry.js
@@ -1,15 +1,13 @@
-'use strict';
-
 import Registry from '../util/Registry'
 
-function ComponentRegistry(entries) {
-  Registry.call(this, entries, function(ComponentClass) {
-    if (!ComponentClass.prototype._isComponent) {
-      throw new Error('Component registry: wrong type. Expected a ComponentClass. Was: ' + String(ComponentClass));
-    }
-  });
+class ComponentRegistry extends Registry {
+  constructor(entries) {
+    super(entries, function(ComponentClass) {
+      if (!ComponentClass.prototype._isComponent) {
+        throw new Error('Component registry: wrong type. Expected a ComponentClass. Was: ' + String(ComponentClass))
+      }
+    })
+  }
 }
 
-Registry.extend(ComponentRegistry);
-
-export default ComponentRegistry;
+export default ComponentRegistry
