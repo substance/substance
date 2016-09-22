4483e30b9a7aad5ea5d0b728f3ea61664008079c
Daniel Beilinson <beilinson@gmail.com>
2016-09-22 20:46:48 +0300

    ES6 FontAwesome.

--- ui/FontAwesomeIconProvider.js
+++ ui/FontAwesomeIconProvider.js
@@ -1,33 +1,30 @@
-'use strict';
-
 import forEach from 'lodash/forEach'
 import oo from '../util/oo'
 import Icon from './FontAwesomeIcon'
 
-function FontAwesomeIconProvider(icons) {
-  this.map = {};
-  forEach(icons, function(config, name) {
-    var faClass = config['fontawesome'];
-    if (faClass) {
-      this.addIcon(name, faClass);
+class FontAwesomeIconProvider {
+  constructor(icons) {
+    this.map = {}
+    forEach(icons, function(config, name) {
+      let faClass = config['fontawesome']
+      if (faClass) {
+        this.addIcon(name, faClass)
+      }
+    }.bind(this))
+  }
+
+  renderIcon($$, name) {
+    let iconClass = this.map[name]
+    if (iconClass) {
+      return $$(Icon, {icon:iconClass})
     }
-  }.bind(this));
+  }
+
+  addIcon(name, faClass) {
+    this.map[name] = faClass
+  }
 }
 
-FontAwesomeIconProvider.Prototype = function() {
+oo.initClass(FontAwesomeIconProvider)
 
-  this.renderIcon = function($$, name) {
-    var iconClass = this.map[name];
-    if (iconClass) {
-      return $$(Icon, {icon:iconClass});
-    }
-  };
-
-  this.addIcon = function(name, faClass) {
-    this.map[name] = faClass;
-  };
-};
-
-oo.initClass(FontAwesomeIconProvider);
-
-export default FontAwesomeIconProvider;
+export default FontAwesomeIconProvider
