50e27933471743c2aced9aaedf3fc9a76469adb4
Daniel Beilinson <beilinson@gmail.com>
2016-09-22 18:50:48 +0300

    ES6 DragManager.

--- ui/DragManager.js
+++ ui/DragManager.js
@@ -1,120 +1,128 @@
-'use strict';
-
 import oo from '../util/oo'
 import DefaultDOMElement from './DefaultDOMElement'
 import Component from './Component'
 
-function DragManager(dndHandlers, context) {
-  this.context = context;
-  this.dndHandlers = dndHandlers;
+class DragManager {
+  constructor(dndHandlers, context) {
+    this.context = context
+    this.dndHandlers = dndHandlers
 
-  this._source = null;
-}
+    this._source = null
+  }
 
-DragManager.Prototype = function() {
+  dispose() {
+    let documentEl = DefaultDOMElement.wrapNativeElement(window.document)
+    documentEl.off(this)
+  }
 
-  this.dispose = function() {
-    var documentEl = DefaultDOMElement.wrapNativeElement(window.document);
-    documentEl.off(this);
-  };
-
-  this.onDragStart = function(event, component) { // eslint-disable-line
+  onDragStart(event, component) { // eslint-disable-line
     // dito: trigger listeners to expose drop targets
     // console.log('DragManager.onDragStart');
-    event.dataTransfer.effectAllowed = 'all';
-    event.dataTransfer.setData('text/html', event.target.outerHTML);
-    event.stopPropagation();
+    event.dataTransfer.effectAllowed = 'all'
+    event.dataTransfer.setData('text/html', event.target.outerHTML)
+    event.stopPropagation()
 
     this._source = {
       component: component
-    };
-
-    for (var i = 0; i < this.dndHandlers.length; i++) {
-      var handler = this.dndHandlers[i];
-      handler.dragStart(this._source, this.context);
     }
-  };
 
-  this.onDragEnter = function(event, component) { // eslint-disable-line
+    for (let i = 0; i < this.dndHandlers.length; i++) {
+      let handler = this.dndHandlers[i]
+      handler.dragStart(this._source, this.context)
+    }
+  }
+
+  onDragEnter(event, component) { // eslint-disable-line
     // we could emit an event, so that listeners could expose drop targets
     // console.log('DragManager.onDragEnter', event);
-    event.stopPropagation();
-  };
+    event.stopPropagation()
+  }
 
-  this.onDragOver = function(event, component) { // eslint-disable-line
+  onDragOver(event, component) { // eslint-disable-line
     // prevent default to allow drop
-    event.preventDefault();
-    event.stopPropagation();
-  };
+    event.preventDefault()
+    event.stopPropagation()
+  }
 
-  this.onDrop = function(event, component) {
-    event.preventDefault();
-    event.stopPropagation();
+  onDrop(event, component) {
+    event.preventDefault()
+    event.stopPropagation()
     // console.log('DragManager.onDragEnter', event);
-    var params = {
+    let params = {
       source: this._source,
       target: _getTargetInfo(event, component),
       data: _getData(event)
-    };
-    var i, handler;
+    }
+    let i, handler;
     for (i = 0; i < this.dndHandlers.length; i++) {
-      handler = this.dndHandlers[i];
-      var _break = handler.drop(params, this.context);
-      if (_break) break;
+      handler = this.dndHandlers[i]
+      let _break = handler.drop(params, this.context)
+      if (_break) break
     }
     for (i = 0; i < this.dndHandlers.length; i++) {
-      handler = this.dndHandlers[i];
-      handler.dragEnd(params, this.context);
+      handler = this.dndHandlers[i]
+      handler.dragEnd(params, this.context)
     }
-  };
+  }
 
-  function _getData(event) {
-    var dataTransfer = event.dataTransfer;
+  _getData(event) {
+    let dataTransfer = event.dataTransfer
     if (dataTransfer) {
       return {
         types: dataTransfer.types,
         items: Array.prototype.slice.call(dataTransfer.items),
         files: Array.prototype.slice.call(dataTransfer.files)
-      };
+      }
     }
   }
 
-  function _getTargetInfo(event) {
-    var target = {
-      element: DefaultDOMElement.wrapNativeElement(event.target)
-    };
-    // try to get information about the component
-    var comp = Component.getComponentFromNativeElement(event.target);
-    comp = _getComponent(comp);
-    if (comp) {
-      target.comp = comp;
-      if (target._isSurface) {
-        target.surface = comp;
-      } else if (comp.context.surface) {
-        target.surface = comp.context.surface;
-      }
-      if (target.surface) {
-        var sel = target.surface.getSelectionFromEvent(event);
-        if (sel) target.selection = sel;
-      }
-      var node = comp.props.node;
-      if (node) target.node = node;
-      if (comp._isTextPropertyComponent) {
-        target.path = comp.props.path;
-      }
+}
+
+function _getData(event) {
+  let dataTransfer = event.dataTransfer
+  if (dataTransfer) {
+    return {
+      types: dataTransfer.types,
+      items: Array.prototype.slice.call(dataTransfer.items),
+      files: Array.prototype.slice.call(dataTransfer.files)
     }
-    return target;
   }
+}
 
-  function _getComponent(comp) {
-    if (comp._isTextNodeComponent || comp._isElementComponent) {
-      return _getComponent(comp.getParent());
+function _getTargetInfo(event) {
+  let target = {
+    element: DefaultDOMElement.wrapNativeElement(event.target)
+  }
+  // try to get information about the component
+  let comp = Component.getComponentFromNativeElement(event.target)
+  comp = _getComponent(comp)
+  if (comp) {
+    target.comp = comp
+    if (target._isSurface) {
+      target.surface = comp
+    } else if (comp.context.surface) {
+      target.surface = comp.context.surface
     }
-    return comp;
+    if (target.surface) {
+      let sel = target.surface.getSelectionFromEvent(event)
+      if (sel) target.selection = sel
+    }
+    let node = comp.props.node
+    if (node) target.node = node
+    if (comp._isTextPropertyComponent) {
+      target.path = comp.props.path
+    }
   }
+  return target
+}
 
-};
+function _getComponent(comp) {
+  if (comp._isTextNodeComponent || comp._isElementComponent) {
+    return _getComponent(comp.getParent())
+  }
+  return comp;
+}
 
-oo.initClass(DragManager);
+oo.initClass(DragManager)
 
-export default DragManager;
+export default DragManager
