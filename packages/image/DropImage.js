b2266d0f88632e2c94c99481520ae91e99653a37
Daniel Beilinson <beilinson@gmail.com>
2016-09-22 18:44:07 +0300

    ES6 DragAndDropHandler.

--- packages/image/DropImage.js
+++ packages/image/DropImage.js
@@ -1,37 +1,31 @@
-'use strict';
-
 import startsWith from 'lodash/startsWith'
 import DragAndDropHandler from '../../ui/DragAndDropHandler'
 import InsertImageCommand from './InsertImageCommand'
 
-function DropImage() {}
+class DropImage extends DragAndDropHandler {
 
-DropImage.Prototype = function() {
-
-  this.drop = function(params, context) {
-    var target = params.target;
+  drop(params, context) {
+    let target = params.target
     // precondition: we need a surface and a selection
     // and act only if there are image files
-    var surface = target.surface;
-    var selection = target.selection;
-    var files = params.data.files;
-    if (!surface || !selection || !files || files.length === 0) return;
+    let surface = target.surface
+    let selection = target.selection
+    let files = params.data.files
+    if (!surface || !selection || !files || files.length === 0) return
     // pick only the images
     files = files.filter(function(file) {
-      return startsWith(file.type, 'image');
-    });
-    if (files.length === 0) return;
+      return startsWith(file.type, 'image')
+    })
+    if (files.length === 0) return
     context.commandManager.executeCommand(InsertImageCommand.type, {
       surface: surface,
       selection: selection,
       files: files
-    });
+    })
     // this lets DropManager know that drop was handled
-    return true;
-  };
+    return true
+  }
 
-};
+}
 
-DragAndDropHandler.extend(DropImage);
-
-export default DropImage;
+export default DropImage
