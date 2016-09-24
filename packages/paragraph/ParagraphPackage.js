bc83d5cd257e81248f4843e19e0268e36e52f500
Daniel Beilinson <beilinson@gmail.com>
2016-09-24 23:12:23 +0300

    ES6 paragraph package.

--- packages/paragraph/ParagraphPackage.js
+++ packages/paragraph/ParagraphPackage.js
@@ -1,32 +1,29 @@
-'use strict';
-
 import Paragraph from './Paragraph'
 import ParagraphComponent from './ParagraphComponent'
 import ParagraphHTMLConverter from './ParagraphHTMLConverter'
 import ParagraphXMLConverter from './ParagraphXMLConverter'
 
-
 export default {
   name: 'paragraph',
   configure: function(config) {
-    config.addNode(Paragraph);
-    config.addComponent(Paragraph.type, ParagraphComponent);
-    config.addConverter('html', ParagraphHTMLConverter);
-    config.addConverter('xml', ParagraphXMLConverter);
+    config.addNode(Paragraph)
+    config.addComponent(Paragraph.type, ParagraphComponent)
+    config.addConverter('html', ParagraphHTMLConverter)
+    config.addConverter('xml', ParagraphXMLConverter)
     config.addTextType({
       name: 'paragraph',
       data: {type: 'paragraph'}
-    });
+    })
     config.addLabel('paragraph', {
       en: 'Paragraph',
       de: 'Paragraph'
-    });
+    })
     config.addLabel('paragraph.content', {
       en: 'Paragraph',
       de: 'Paragraph'
-    });
+    })
   },
   Paragraph: Paragraph,
   ParagraphComponent: ParagraphComponent,
   ParagraphHTMLConverter: ParagraphHTMLConverter
-};
+}
