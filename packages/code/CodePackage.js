b80faa06438582b485286c44b322fe61c3462aad
Daniel Beilinson <beilinson@gmail.com>
2016-09-24 21:38:38 +0300

    ES6 code package.

--- packages/code/CodePackage.js
+++ packages/code/CodePackage.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import Code from './Code'
 import CodeHTMLConverter from './CodeHTMLConverter'
 import CodeXMLConverter from './CodeXMLConverter'
@@ -11,17 +9,17 @@ export default {
   name: 'code',
   configure: function(config) {
     config.addNode(Code);
-    config.addConverter('html', CodeHTMLConverter);
-    config.addConverter('xml', CodeXMLConverter);
-    config.addComponent('code', AnnotationComponent);
-    config.addCommand('code', AnnotationCommand, { nodeType: Code.type });
-    config.addTool('code', AnnotationTool, {target: 'annotations'});
-    config.addIcon('code', { 'fontawesome': 'fa-code' });
+    config.addConverter('html', CodeHTMLConverter)
+    config.addConverter('xml', CodeXMLConverter)
+    config.addComponent('code', AnnotationComponent)
+    config.addCommand('code', AnnotationCommand, { nodeType: Code.type })
+    config.addTool('code', AnnotationTool, {target: 'annotations'})
+    config.addIcon('code', { 'fontawesome': 'fa-code' })
     config.addLabel('code', {
       en: 'Code',
       de: 'Code'
-    });
+    })
   },
   Code: Code,
   CodeHTMLConverter: CodeHTMLConverter
-};
+}
