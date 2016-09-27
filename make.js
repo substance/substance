1a94add7f3b7eec0ee2751a292755ac6376a0692
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-09-27 14:09:53 +0200

    Enabling karma and 'npm test'

--- make.js
+++ make.js
@@ -11,7 +11,6 @@ b.task('css', function() {
 
 b.task('browser', function() {
   b.js('./index.es.js', {
-    buble: true,
     ignore: ['substance-cheerio'],
     commonjs: { include: ['node_modules/lodash/**'] },
     targets: [{
@@ -23,48 +22,36 @@ b.task('browser', function() {
 
 b.task('server', function() {
   b.js('./index.es.js', {
-    buble: true,
     commonjs: { include: ['node_modules/**', require.resolve('substance-cheerio')] },
     targets: [{
       dest: './dist/substance.cjs.js',
       format: 'cjs', sourceMapRoot: __dirname, sourceMapPrefix: 'substance'
-    }, {
-      dest: './dist/substance.es6.js',
-      format: 'es', sourceMapRoot: __dirname, sourceMapPrefix: 'substance'
     }]
   });
 });
 
-// NEXT TODO: make test suite run in browser again, i.e. bundle the files for the browser test suite
-// in substance-test and deploy these to tmp folder
-
 b.task('test:clean', function() {
-  b.rm('tmp')
+  b.rm('dist/test')
 })
 
 b.task('test:assets', function() {
-  b.copy('./node_modules/substance-test/dist/*', './tmp', { root: './node_modules/substance-test/dist' })
+  b.copy('./node_modules/substance-test/dist/*', './dist/test', { root: './node_modules/substance-test/dist' })
 })
 
 b.task('test:browser', function() {
   b.js('./test/index.js', {
-    buble: true,
     ignore: ['substance-cheerio'],
     external: ['substance-test'],
-    commonjs: {
-      include: [
-        'node_modules/lodash/**',
-      ]
-    },
+    commonjs: { include: ['node_modules/lodash/**'] },
     targets: [
-      { dest: './tmp/tests.js', format: 'umd', moduleName: 'tests' }
+      { dest: './dist/test/tests.js', format: 'umd', moduleName: 'tests' }
     ]
   });
 })
 
 b.task('test:server', function() {
-  b.copy('./node_modules/substance-test/dist/*', './tmp', { root: './node_modules/substance-test/dist' })
   b.js('./test/index.js', {
+    // buble necessary here, for nodejs
     buble: true,
     external: ['substance-test'],
     commonjs: {
@@ -74,11 +61,17 @@ b.task('test:server', function() {
       ]
     },
     targets: [
-      { dest: './tmp/run-tests.js', format: 'cjs' },
+      { dest: './dist/test/run-tests.js', format: 'cjs' },
     ]
   });
 })
 
 b.task('test', ['test:clean', 'test:assets', 'test:browser', 'test:server'])
+
+// starts a server when CLI argument '-s' is set
+b.setServerPort(5550)
+b.serve({
+  static: true, route: '/', folder: 'dist'
+})
 
 b.task('default', ['clean', 'css', 'browser', 'server'])
