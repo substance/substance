ea9b1ac45d8af7b5d531e4dd495990eea600d26e
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-10-30 15:51:01 +0100

    Fix regression.

--- ui/BrowserDOMElement.js
+++ ui/BrowserDOMElement.js
@@ -93,7 +93,7 @@ class BrowserDOMElement extends DOMElement {
   }
 
   setTagName(tagName) {
-    let newEl = this.el.createElement(tagName)
+    let newEl = this.createElement(tagName)
     let attributes = this.el.attributes
     let l = attributes.length
     let i
