45a629e45f438ecf1a1ff7d80f382b6eb3756eb2
Daniel Beilinson <beilinson@gmail.com>
2016-12-07 15:08:41 +0300

    Fix content-type header.

--- util/request.js
+++ util/request.js
@@ -18,7 +18,7 @@
 function request(method, url, data, cb) {
   var request = new XMLHttpRequest()
   request.open(method, url, true)
-  request.setRequestHeader('Content-Type', 'application/json charset=UTF-8')
+  request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
       var res = request.responseText
