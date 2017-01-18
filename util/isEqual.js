d498846bd4be8598de0122a0e2a54c413c2cd7a4
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-01-18 16:46:57 +0100

    Added util/isEqual

--- util/isEqual.js
+++ util/isEqual.js
@@ -1,1 +1,24 @@
-export { default } from 'lodash-es/isEqual'
+import isNil from './isNil'
+import isPlainObject from './isPlainObject'
+import isArray from './isArray'
+
+export default function isEqual(a, b) {
+  if (a === b) return true
+  if (isArray(a) && isArray(b)) {
+    if (a.length !== b.length) return false
+    for (let i = 0; i < a.length; i++) {
+      if (!isEqual(a[i], b[i])) return false
+    }
+    return true
+  }
+  if (isPlainObject(a) && isPlainObject(b)) {
+    let akeys = Object.keys(a).sort()
+    let bkeys = Object.keys(b).sort()
+    if (!isEqual(akeys, bkeys)) return false
+    for (let key of akeys) {
+      if (!isEqual(a[key], b[key])) return false
+    }
+    return true
+  }
+  return false
+}
