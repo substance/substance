024a4b4a07e5cd40dfa03f3deca2f489909b60c9
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-02-10 00:33:26 +0100

    Fix util/filter

--- util/filter.js
+++ util/filter.js
@@ -2,6 +2,7 @@ import isFunction from './isFunction'
 import forEach from './forEach'
 
 export default function filter(iteratee, fn) {
+  if (!iteratee) return []
   if (iteratee.constructor.prototype.filter && isFunction(iteratee.constructor.prototype.filter)) {
     return iteratee.filter(fn)
   }
