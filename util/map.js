b2f053ef3804c3b02e4165612158752c5f7fe89a
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-03-09 20:08:36 +1300

    Remove special object map support.

--- util/map.js
+++ util/map.js
@@ -3,13 +3,6 @@ export default function map(iteratee, func) {
   if (!func) func = function(item) { return item }
   if (Array.isArray(iteratee)) {
     return iteratee.map(func)
-  } else if (typeof iteratee.length !== 'undefined') {
-    let l = iteratee.length
-    let result = []
-    for (var i = 0; i < l; i++) {
-      result.push(func(iteratee[i], i))
-    }
-    return result
   } else {
     return Object.keys(iteratee).map(function(key) {
       return func(iteratee[key], key)
