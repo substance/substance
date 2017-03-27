9b4a7be108b93f983efda60968b01391f2cf2a3c
Tobias Södergren <tobias.sodergren@infomaker.se>
2017-03-27 14:01:19 +0200

    Added config option to force plain-text paste

--- ui/Clipboard.js
+++ ui/Clipboard.js
@@ -28,7 +28,8 @@ class Clipboard {
     let _config = {
       schema: schema,
       DocumentClass: doc.constructor,
-      converters: htmlConverters
+      converters: htmlConverters,
+      editorOptions: config.editorOptions
     }
 
     this.htmlImporter = new ClipboardImporter(_config)
