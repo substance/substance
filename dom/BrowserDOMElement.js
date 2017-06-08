61fede3f17e45fee72b7666df4f6987a8dd2f7f6
Michael Aufreiter <michael@substance.io>
2017-06-08 15:31:46 +0200

    Use 'application/xml' when parsing XML

--- dom/BrowserDOMElement.js
+++ dom/BrowserDOMElement.js
@@ -649,7 +649,7 @@ BrowserDOMElement.parseMarkup = function(str, format, options={}) {
   } else if (format === 'xml') {
     doc = BrowserDOMElement.wrap(
       _check(
-        parser.parseFromString(str, 'text/xml')
+        parser.parseFromString(str, 'application/xml')
       )
     )
   }
