1c1cb2c1c7a579e73c189fb59cd08fb2ccd20269
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-06-16 20:45:37 +0300

    Fix InlineNodeComponent implementation

--- test/fixture/TestInlineNodeComponent.js
+++ test/fixture/TestInlineNodeComponent.js
@@ -1,17 +1,6 @@
-import { InlineNodeComponent } from 'substance'
+import { InlineNodeComponent, TextPropertyEditor } from 'substance'
 
 class TestInlineNodeComponent extends InlineNodeComponent {
-  didMount() {
-    super.didMount()
-    this.context.editorSession.onRender('document', this.rerender, this, {
-      path: [this.props.node.id, 'content']
-    })
-  }
-
-  dispose() {
-    super.dispose()
-    this.context.editorSession.off(this)
-  }
 
   getClassNames() {
     // ATTENTION: ATM it is necessary to add .sc-inline-node
@@ -19,7 +8,24 @@ class TestInlineNodeComponent extends InlineNodeComponent {
   }
 
   renderContent($$) {
-    return $$('span').append(this.props.node.content)
+    const node = this.props.node
+    // FIXME: this is not working properly
+    // we should pass down `disabled: true`
+    // if the node is not focused
+    // But then, we can not put the selection inside
+    // const disabled = this.isDisabled()
+    if (this.props.node.id === 'in2') {
+      console.log('in2 disabled?', this.isDisabled())
+    }
+    const disabled = false
+    return $$('span').append(
+      $$(TextPropertyEditor, {
+        tagName: 'span',
+        path: [node.id, 'content'],
+        withoutBreak: true,
+        disabled: this.isDisabled()
+      }).ref('editor')
+    )
   }
 }
 
