14464d772782aa1e5a2088d7a8dd0559df001b28
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-06-23 20:42:22 +0200

    Refactored internal RNG compilation

---
+++ xml/_lookupRNG.js
@@ -0,0 +1,8 @@
+export default function _lookupRNG(fs, searchDirs, file) {
+  for (let i = 0; i < searchDirs.length; i++) {
+    let absPath = searchDirs[i] + '/' + file
+    if (fs.existsSync(absPath)) {
+      return absPath
+    }
+  }
+}
