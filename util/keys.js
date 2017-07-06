9c3066fc0f22d644f87dfaf579dcae77a8aeaf9e
Daniel Beilinson <beilinson@gmail.com>
2017-07-06 23:56:56 +0300

    Add more key codes.

--- util/keys.js
+++ util/keys.js
@@ -1,13 +1,15 @@
 /* eslint-disable strict */
-export default {
+let keys = {
   UNDEFINED: 0,
   BACKSPACE: 8,
   DELETE: 46,
+  INSERT: 45,
   LEFT: 37,
   RIGHT: 39,
   UP: 38,
   DOWN: 40,
   ENTER: 13,
+  RETURN: 13,
   END: 35,
   HOME: 36,
   TAB: 9,
@@ -16,5 +18,17 @@ export default {
   ESCAPE: 27,
   ESC: 27,
   SHIFT: 16,
-  SPACE: 32
-};
+  SPACE: 32,
+  PLUS: 171,
+  VOLUMEUP: 183,
+  VOLUMEDOWN: 182,
+  VOLUMEMUTE: 181,
+  PRINTSCREEN: 44
+}
+
+// Handle F1 to F24 keys
+for(let i=1;i<=24;i++) {
+  keys['F'+i] = 111 + i
+}
+
+export default keys
