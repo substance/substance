5fcd64efa9a97e12834f29b501b455f014777622
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-07-07 13:34:24 +0200

    Adding an initial test case for KeyboardManager

---
+++ util/parseKeyEvent.js
@@ -0,0 +1,19 @@
+export {default as keys} from './keys'
+
+export default function parseKeyEvent(event, onlyModifiers) {
+  let frags = []
+  if (event.altKey) {
+    if (event.code === 'AltRight') {
+      frags.push('ALTGR')
+    } else {
+      frags.push('ALT')
+    }
+  }
+  if (event.ctrlKey) frags.push('CTRL')
+  if (event.metaKey) frags.push('META')
+  if (event.shiftKey) frags.push('SHIFT')
+  if (!onlyModifiers) {
+    frags.push(event.keyCode)
+  }
+  return frags.join('+')
+}
