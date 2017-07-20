cfebdbaace1824f35a7a03e5136cc623bae109f5
Daniel Beilinson <beilinson@gmail.com>
2017-07-20 17:53:57 +0300

    Basic CommandManager test.

---
+++ test/CommandManager.test.js
@@ -0,0 +1,34 @@
+import { module } from 'substance-test'
+import simple from './fixture/simple'
+import setupEditor from './fixture/setupEditor'
+
+const test = module('CommandManager')
+
+test("Command state changing with selection", (t) => {
+  let { editorSession } = setupEditor(t, simple)
+  editorSession.setSelection({
+    type: 'property',
+    path: ['p1', 'content'],
+    startOffset: 3,
+    containerId: 'body'
+  })
+  
+  let commandStates = editorSession.getCommandStates()
+  t.equal(commandStates.paragraph.active, true, 'Paragraph should be active for collapsed selection')
+  t.equal(commandStates.paragraph.disabled, false, 'Paragraph should not be disabled for collapsed selection')
+  t.equal(commandStates.strong.active, false, 'Strong should not be active for collapsed selection')
+  t.equal(commandStates.strong.disabled, true, 'Strong should be disabled for collapsed selection')
+  t.end()
+
+  editorSession.setSelection({
+    type: 'property',
+    path: ['p1', 'content'],
+    startOffset: 3,
+    endOffset: 4,
+    containerId: 'body'
+  })
+
+  commandStates = editorSession.getCommandStates()
+  t.equal(commandStates.strong.active, false, 'Strong should not be active for non-collapsed selection without strong annotation')
+  t.equal(commandStates.strong.disabled, false, 'Strong should not be disabled for non-collapsed selection')
+})
