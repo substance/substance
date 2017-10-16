904f80ef17b512bee6152b895933e4afe4977870
Michael Aufreiter <michael@substance.io>
2017-10-16 19:24:12 +0200

    Allow simple templates for labels.

--- ui/DefaultLabelProvider.js
+++ ui/DefaultLabelProvider.js
@@ -7,10 +7,37 @@ class DefaultLabelProvider {
     this.labels = labels
   }
 
-  getLabel(name) {
+  getLabel(name, context) {
     let labels = this.labels[this.lang]
     if (!labels) return name
-    return labels[name] || name
+    let rawLabel = labels[name] || name
+    // If context is provided, resolve templates
+    if (context) {
+      return this._evalTemplate(rawLabel, context)
+    } else {
+      return rawLabel
+    }
+  }
+
+  _evalTemplate(label, context) {
+    let vars = this._extractVariables(label)
+    vars.forEach((varName) => {
+      let searchExp = new RegExp(`\\\${${varName}}`, 'g')
+      let replaceStr = context[varName]
+      label = label.replace(searchExp, replaceStr)
+    })
+    return label
+  }
+
+  _extractVariables(rawLabel) {
+    let qualityRegex = /\${(\w+)}/g
+    let matches
+    let vars = []
+
+    while (matches = qualityRegex.exec(rawLabel)) { // eslint-disable-line
+      vars.push(matches[1])
+    }
+    return vars
   }
 
   hasLabel(name) {
