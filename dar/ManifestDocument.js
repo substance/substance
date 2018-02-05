22646e023f2a88f2d2cb2fb178556946e6ec59e5
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-02-05 13:28:54 +0100

    Fix ManifestDocument

--- dar/ManifestDocument.js
+++ dar/ManifestDocument.js
@@ -1,17 +1,36 @@
 import { XMLDocument } from '../xml'
+import ManifestSchema from './ManifestSchema'
 
 export default class ManifestDocument extends XMLDocument {
+
   getRootNode() {
     if (!this.root) {
       let nodes = this.getNodes()
       let ids = Object.keys(nodes)
       for (var i = 0; i < ids.length; i++) {
         let node = nodes[ids[i]]
-        if (node.type === 'container') {
+        if (node.type === 'archive') {
           this.root = node
         }
       }
     }
     return this.root
   }
+
+  getDocTypeParams() {
+    return ManifestSchema.getDocTypeParams()
+  }
+
+  getXMLSchema() {
+    return ManifestSchema
+  }
+
+  getDocumentNodes() {
+    return this.findAll('documents > document')
+  }
+
+  getAssetNodes() {
+    return this.findAll('assets > asset')
+  }
+
 }
