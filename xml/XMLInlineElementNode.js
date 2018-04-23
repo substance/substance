1f1af79bca98d2f1d0918422ff7ee7fd5bdce3ad
Daniel <beilinson@gmail.com>
2018-04-23 16:17:34 +0300

    Append and remove child API for inline nodes.

--- xml/XMLInlineElementNode.js
+++ xml/XMLInlineElementNode.js
@@ -25,6 +25,42 @@ class XMLInlineElementNode extends XMLAnnotationNode {
     this._parentNode = parent
   }
 
+  appendChild(child) {
+    this.insertAt(this._childNodes.length, child)
+  }
+
+  removeChild(child) {
+    const childId = child.id
+    const childPos = this._childNodes.indexOf(childId)
+    if (childPos >= 0) {
+      this.removeAt(childPos)
+    } else {
+      throw new Error(`node ${childId} is not a child of ${this.id}`)
+    }
+    return this
+  }
+
+  insertAt(pos, child) {
+    const length = this._childNodes.length
+    if (pos >= 0 && pos <= length) {
+      const doc = this.getDocument()
+      doc.update([this.id, '_childNodes'], { type: 'insert', pos, value: child.id })
+    } else {
+      throw new Error('Index out of bounds.')
+    }
+    return this
+  }
+
+  removeAt(pos) {
+    const length = this._childNodes.length
+    if (pos >= 0 && pos < length) {
+      const doc = this.getDocument()
+      doc.update([this.id, '_childNodes'], { type: 'delete', pos: pos })
+    } else {
+      throw new Error('Index out of bounds.')
+    }
+    return this
+  }
 }
 
 XMLInlineElementNode.prototype._elementType = 'inline-element'
