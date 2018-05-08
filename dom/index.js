accc29d1b6d33d203fa8294587395aeee7cb81e3
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-05-08 14:11:37 +0200

    Expose domHelpers as a collection

--- dom/index.js
+++ dom/index.js
@@ -3,10 +3,10 @@ import DOMEventListener from './DOMEventListener'
 import DefaultDOMElement from './DefaultDOMElement'
 import BrowserDOMElement from './BrowserDOMElement'
 import MemoryDOMElement from './MemoryDOMElement'
+import * as domHelpers from './domHelpers'
 
 export {
   DOMElement, DOMEventListener, DefaultDOMElement,
-  BrowserDOMElement, MemoryDOMElement
+  BrowserDOMElement, MemoryDOMElement,
+  domHelpers
 }
-
-export * from './domHelpers'
