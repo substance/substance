c6dd26c8747b56418499082b092cccff74faf3e5
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-05-29 22:08:30 +0200

    Allow to map Maps and Sets

--- util/map.js
+++ util/map.js
@@ -3,9 +3,23 @@ export default function map (iteratee, func) {
   if (!func) func = function (item) { return item }
   if (Array.isArray(iteratee)) {
     return iteratee.map(func)
-  } else {
-    return Object.keys(iteratee).map(function (key) {
-      return func(iteratee[key], key)
-    })
   }
+  if (iteratee instanceof Map) {
+    let result = []
+    for (let [name, val] of iteratee) {
+      result.push(func(val, name))
+    }
+    return result
+  }
+  if (iteratee instanceof Set) {
+    let result = []
+    let idx = 0
+    iteratee.forEach(item => {
+      result.push(func(item, idx++))
+    })
+    return result
+  }
+  return Object.keys(iteratee).map(function (key) {
+    return func(iteratee[key], key)
+  })
 }
