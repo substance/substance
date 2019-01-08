495a76fe4a0a576fdb3d0b191df10c5b7080d96a
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-01-08 23:08:14 +0100

    Flatten schema targetTypes.

--- model/schemaHelpers.js
+++ model/schemaHelpers.js
@@ -1,6 +1,9 @@
+import flatten from '../util/flatten'
+
 export const STRING = { type: 'string', default: '' }
 
 export function TEXT (...targetTypes) {
+  targetTypes = flatten(targetTypes)
   return { type: 'text', targetTypes }
 }
 
@@ -11,20 +14,24 @@ export const STRING_ARRAY = { type: ['array', 'string'], default: [] }
 export const BOOLEAN = { type: 'boolean', default: false }
 
 export function MANY (...targetTypes) {
+  targetTypes = flatten(targetTypes)
   return { type: ['array', 'id'], targetTypes, default: [] }
 }
 
 export function ONE (...targetTypes) {
+  targetTypes = flatten(targetTypes)
   return { type: 'id', targetTypes, default: null }
 }
 
 export function CHILDREN (...targetTypes) {
+  targetTypes = flatten(targetTypes)
   return { type: ['array', 'id'], targetTypes, default: [], owned: true }
 }
 
 // EXPERIMENTAL: similar to CHILDREN but only a single id, e.g. figure.content -> graphic
 // for now we make this non-optional
 export function CHILD (...targetTypes) {
+  targetTypes = flatten(targetTypes)
   return { type: 'id', targetTypes, owned: true }
 }
 
