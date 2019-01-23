6a075a4d4bb315fbb18594e869c6a9ca12fab883
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-01-23 09:31:30 +0100

    Tidy up.

--- ui/TextPropertyEditor.js
+++ ui/TextPropertyEditor.js
@@ -1,30 +1,23 @@
 import Surface from './Surface'
 
 /**
-  Editor for a text property (annotated string). Needs to be
-  instantiated inside a {@link ui/Controller} context.
-
-  @class
-  @component
-  @extends ui/Surface
-
-  @prop {String} name unique editor name
-  @prop {String[]} path path to a text property
-  @prop {ui/SurfaceCommand[]} commands array of command classes to be available
-
-  @example
-
-  Create a `TextPropertyEditor` for the `name` property of an author object. Allow emphasis annotations.
-
-  ```js
-  $$(TextPropertyEditor, {
-    name: 'authorNameEditor',
-    path: ['author_1', 'name'],
-    commands: [EmphasisCommand]
-  })
-  ```
-*/
-
+ * Editor for a text property (annotated string). Needs to be
+ * instantiated inside a {@link ui/Controller} context.
+ *
+ * @param {string} props.name unique editor name
+ * @param {string[]} props.path path to a text property
+ *
+ * @example
+ *
+ * Create a `TextPropertyEditor` for the `name` property of an author object.
+ *
+ * ```js
+ * $$(TextPropertyEditor, {
+ *   name: 'authorNameEditor',
+ *   path: ['author_1', 'name']
+ * })
+ * ```
+ */
 export default class TextPropertyEditor extends Surface {
   constructor (parent, props) {
     // making props.name optional
@@ -40,7 +33,6 @@ export default class TextPropertyEditor extends Surface {
     const TextPropertyComponent = this.getComponent('text-property')
 
     let el = super.render.apply(this, arguments)
-    el.addClass('sc-text-property-editor')
 
     if (!this.props.disabled) {
       el.addClass('sm-enabled')
@@ -61,6 +53,10 @@ export default class TextPropertyEditor extends Surface {
     )
 
     return el
+  }
+
+  _getClassNames () {
+    return 'sc-text-property-editor sc-surface'
   }
 
   selectFirst () {
