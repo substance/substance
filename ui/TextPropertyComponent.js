8a258bd8dbb893428909abcc70baca8d3f753f1b
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-04-23 18:03:06 +0200

    Let TextPropertyComponent more customizable.
    Instead of using functions from the module closure, use class member functions.

--- ui/TextPropertyComponent.js
+++ ui/TextPropertyComponent.js
@@ -203,147 +203,141 @@ export default class TextPropertyComponent extends AnnotatedTextComponent {
     rendered by the TextPropertyComponent
   */
   static getCoordinate (root, el, offset) {
-    let context = _getPropertyContext(root, el, offset)
+    let context = this._getPropertyContext(root, el, offset)
     if (!context) {
       return null
     }
-    // EXPERIMENTAL: trying to detect
-    // TODO: add tests and remove this comment when mature
-    let charPos
-    if (el.parentNode && el.parentNode.is('.se-placeholder')) {
-      charPos = 0
-    } else {
-      // in some cases we need to normalize the DOM coordinate
-      // before we can use it for retrieving charPos
-      // E.g. observed with #273
-      charPos = _getCharPos(context.node, context.offset)
-    }
+    let textPropertyComp = context.comp
+    // in some cases we need to normalize the DOM coordinate
+    // before we can use it for retrieving charPos (e.g., observed in #273)
+    let charPos = textPropertyComp._getCharPos(context.node, context.offset)
     if (isNumber(charPos)) {
       let coor = new Coordinate(context.path, charPos)
-      coor._comp = context.comp
+      // TODO: what is this used for?
+      coor._comp = textPropertyComp
       return coor
     } else {
       return null
     }
   }
-}
 
-function _getPropertyContext (root, node, offset) {
-  let result = {
-    comp: null,
-    el: null,
-    path: null,
-    node: node,
-    offset: offset
-  }
-  while (node && node !== root) {
-    if (node.isElementNode()) {
-      let comp = Component.unwrap(node)
-      if (comp && comp._isTextPropertyComponent) {
-        result.comp = comp
-        result.el = node
-        result.path = comp.getPath()
-        return result
+  static _getPropertyContext (root, node, offset) {
+    let result = {
+      comp: null,
+      el: null,
+      path: null,
+      node: node,
+      offset: offset
+    }
+    while (node && node !== root) {
+      if (node.isElementNode()) {
+        let comp = Component.unwrap(node)
+        if (comp && comp._isTextPropertyComponent) {
+          result.comp = comp
+          result.el = node
+          result.path = comp.getPath()
+          return result
+        }
+        // we need to normalize situations where the DOM coordinate
+        // is inside an inline node, which we have observed
+        // can actually happen.
+        if (node.getAttribute('data-inline')) {
+          result.node = node
+          if (offset > 0) {
+            result.offset = 1
+          }
+        }
       }
-      // we need to normalize situations where the DOM coordinate
-      // is inside an inline node, which we have observed
-      // can actually happen.
-      if (node.getAttribute('data-inline')) {
-        result.node = node
-        if (offset > 0) {
-          result.offset = 1
+      node = node.getParent()
+    }
+    return null
+  }
+
+  _getCharPos (node, offset) {
+    let charPos = offset
+    let parent, childIdx
+
+    /*
+      In the following implementation we are exploiting two facts
+      for optimization:
+      - an element with data-path is assumed to be the text property element
+      - an element with data-offset is assumed to be an annotation element
+
+      Particularly, the data-offset property is helpful to get the character position
+      in just one iteration.
+    */
+
+    parent = node.getParent()
+    if (node.isTextNode()) {
+      // TextNode is first child
+      if (node === parent.firstChild) {
+        // ... we can stop if parent is text property
+        let parentPath = parent.getAttribute('data-path')
+        let parentOffset = parent.getAttribute('data-offset')
+        if (parentPath) {
+          charPos = offset
+        // ... and we can stop if parent has an offset hint
+        } else if (parentOffset) {
+          charPos = parseInt(parentOffset, 10) + offset
+        // ... otherwise we count the charPos by recursing up-tree
+        } else {
+          charPos = this._getCharPos(parent, 0) + offset
+        }
+      } else {
+        // the node has a predecessor so we can apply recurse using the child index
+        childIdx = parent.getChildIndex(node)
+        charPos = this._getCharPos(parent, childIdx) + offset
+      }
+    } else if (node.isElementNode()) {
+      let pathStr = node.getAttribute('data-path')
+      let offsetStr = node.getAttribute('data-offset')
+      // if node is the element of a text property, then offset is a child index
+      // up to which we need to sum up all lengths
+      if (pathStr) {
+        charPos = this._countCharacters(node, offset)
+      // similar if node is the element of an annotation, and we can use the
+      // element's offset
+      } else if (offsetStr) {
+        childIdx = parent.getChildIndex(node)
+        charPos = parseInt(offsetStr, 10) + this._countCharacters(node, offset)
+      // for other elements we need to count characters in the child tree
+      // adding the offset of this element which needs to be computed by recursing up-tree
+      } else {
+        childIdx = parent.getChildIndex(node)
+        charPos = this._getCharPos(parent, childIdx) + this._countCharacters(node, offset)
+      }
+    } else {
+      // Unsupported case
+      return null
+    }
+    return charPos
+  }
+
+  _countCharacters (el, maxIdx) {
+    let charPos = 0
+    // inline elements have a length of 1
+    if (el.getAttribute('data-inline')) {
+      return maxIdx === 0 ? 0 : 1
+    }
+    let l = el.getChildCount()
+    if (arguments.length === 1) {
+      maxIdx = l
+    }
+    maxIdx = Math.min(l, maxIdx)
+    for (let i = 0, child = el.getFirstChild(); i < maxIdx; child = child.getNextSibling(), i++) {
+      if (child.isTextNode()) {
+        charPos += child.getTextContent().length
+      } else if (child.isElementNode()) {
+        let length = child.getAttribute('data-length')
+        if (child.getAttribute('data-inline')) {
+          charPos += 1
+        } else if (length) {
+          charPos += parseInt(length, 10)
+        } else {
+          charPos += this._countCharacters(child)
         }
       }
     }
-    node = node.getParent()
+    return charPos
   }
-  return null
-}
-
-function _getCharPos (node, offset) {
-  let charPos = offset
-  let parent, childIdx
-
-  /*
-    In the following implementation we are exploiting two facts
-    for optimization:
-    - an element with data-path is assumed to be the text property element
-    - an element with data-offset is assumed to be an annotation element
-
-    Particularly, the data-offset property is helpful to get the character position
-    in just one iteration.
-  */
-
-  parent = node.getParent()
-  if (node.isTextNode()) {
-    // TextNode is first child
-    if (node === parent.firstChild) {
-      // ... we can stop if parent is text property
-      let parentPath = parent.getAttribute('data-path')
-      let parentOffset = parent.getAttribute('data-offset')
-      if (parentPath) {
-        charPos = offset
-      // ... and we can stop if parent has an offset hint
-      } else if (parentOffset) {
-        charPos = parseInt(parentOffset, 10) + offset
-      // ... otherwise we count the charPos by recursing up-tree
-      } else {
-        charPos = _getCharPos(parent, 0) + offset
-      }
-    } else {
-      // the node has a predecessor so we can apply recurse using the child index
-      childIdx = parent.getChildIndex(node)
-      charPos = _getCharPos(parent, childIdx) + offset
-    }
-  } else if (node.isElementNode()) {
-    let pathStr = node.getAttribute('data-path')
-    let offsetStr = node.getAttribute('data-offset')
-    // if node is the element of a text property, then offset is a child index
-    // up to which we need to sum up all lengths
-    if (pathStr) {
-      charPos = _countCharacters(node, offset)
-    // similar if node is the element of an annotation, and we can use the
-    // element's offset
-    } else if (offsetStr) {
-      childIdx = parent.getChildIndex(node)
-      charPos = parseInt(offsetStr, 10) + _countCharacters(node, offset)
-    // for other elements we need to count characters in the child tree
-    // adding the offset of this element which needs to be computed by recursing up-tree
-    } else {
-      childIdx = parent.getChildIndex(node)
-      charPos = _getCharPos(parent, childIdx) + _countCharacters(node, offset)
-    }
-  } else {
-    // Unsupported case
-    return null
-  }
-  return charPos
-}
-
-function _countCharacters (el, maxIdx) {
-  let charPos = 0
-  // inline elements have a length of 1
-  if (el.getAttribute('data-inline')) {
-    return maxIdx === 0 ? 0 : 1
-  }
-  let l = el.getChildCount()
-  if (arguments.length === 1) {
-    maxIdx = l
-  }
-  maxIdx = Math.min(l, maxIdx)
-  for (let i = 0, child = el.getFirstChild(); i < maxIdx; child = child.getNextSibling(), i++) {
-    if (child.isTextNode()) {
-      charPos += child.getTextContent().length
-    } else if (child.isElementNode()) {
-      let length = child.getAttribute('data-length')
-      if (child.getAttribute('data-inline')) {
-        charPos += 1
-      } else if (length) {
-        charPos += parseInt(length, 10)
-      } else {
-        charPos += _countCharacters(child)
-      }
-    }
-  }
-  return charPos
 }
