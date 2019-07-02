bd8d72e0d413049524b795fd68cc7e405a8fba5a
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-07-02 21:30:27 +0200

    Removing the current real-time collab implementation.

--- test/collab/ChangeStore.test.js
+++
@@ -1,9 +0,0 @@
-import { ChangeStore } from 'substance'
-import testChangeStore from './testChangeStore'
-
-function createChangeStore () {
-  return new ChangeStore()
-}
-
-// Runs the offical backend test suite
-testChangeStore(createChangeStore)
--- test/collab/SnapshotStore.test.js
+++
@@ -1,9 +0,0 @@
-import { SnapshotStore } from 'substance'
-import testSnapshotStore from './testSnapshotStore'
-
-function createEmptySnapshotStore () {
-  return new SnapshotStore()
-}
-
-// Runs the offical backend test suite
-testSnapshotStore(createEmptySnapshotStore)
