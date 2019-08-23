e9aacfb37a5885984855f79c4240b700ebe494d5
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-08-24 00:54:21 +0200

    Add rollup config files.

---
+++ rollup.config.test.js
@@ -0,0 +1,43 @@
+module.exports = function (options = {}) {
+  let target = options.target || 'all'
+  let config = []
+  if (target === 'browser' || target === 'all') {
+    config.push({
+      input: 'test/index.js',
+      output: [{
+        file: 'tmp/tests.js',
+        format: 'umd',
+        name: 'tests',
+        globals: {
+          'substance': 'window.substance',
+          'substance-test': 'window.substanceTest'
+        }
+      }],
+      external: [ 'substance', 'substance-test' ]
+    })
+  }
+  if (target === 'node' || target === 'all') {
+    config.push({
+      input: 'test/index.js',
+      output: {
+        file: 'tmp/tests.cjs.js',
+        format: 'cjs'
+      },
+      external: ['substance', 'substance-test'],
+      plugins: [
+        _patchNodeTest()
+      ]
+    })
+  }
+  return config
+}
+
+function _patchNodeTest () {
+  return {
+    name: 'patch-node-test',
+    generateBundle (options, bundle, isWrite) {
+      let output = bundle['tests.cjs.js']
+      output.code = output.code.replace("'substance'", "'../dist/substance.cjs.js'")
+    }
+  }
+}
