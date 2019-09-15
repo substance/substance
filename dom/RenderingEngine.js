7729312ea51370c3eec97921cf2dc9dc3f3a2fb8
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-09-16 01:49:10 +0200

    Revert "Pass new createElement to old style render($$)."
    This reverts commit 294cc41d17dd5cbbddf6d9de6473fec6bc3c70bd.

--- dom/RenderingEngine.js
+++ dom/RenderingEngine.js
@@ -201,8 +201,6 @@ export default class RenderingEngine {
                 // TODO: are there more HTML attributes that need to be considered here?
                 case 'id': {
                   _attributes[key] = val
-                  // for legacy reasons (some components used this as prop)
-                  _props[key] = val
                   break
                 }
                 default: {
@@ -402,8 +400,7 @@ function _capture (state, vel, mode) {
       let content
       try {
         _setRenderingContext(context)
-        // NOTE: passing RenderingEngine.createVirtualElement as first argument for old-style components
-        content = comp.render(RenderingEngine.createVirtualElement)
+        content = comp.render(context.$$)
       } finally {
         _setRenderingContext(null)
       }
