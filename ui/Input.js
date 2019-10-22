fa6bd2842cdea36ffd1acc008829fc50d6b4f1df
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-10-23 00:14:26 +0200

    Force focus when autofocus is set on Input.

--- ui/Input.js
+++ ui/Input.js
@@ -1,6 +1,14 @@
 import { $$, Component } from '../dom'
 
 export default class Input extends Component {
+  didMount () {
+    // HACK: for whatever reason, the native autofocus does not work
+    // after the first time. This forces focus if autofocus is set
+    if (this.el.getAttribute('autofocus') === 'true') {
+      this.el.focus()
+    }
+  }
+
   render () {
     const attributes = Object.assign({ class: 'sc-input' }, this.props)
     delete attributes.label
