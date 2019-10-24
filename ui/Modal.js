8872216920b2144e7fa15dc9578c6086adf6c913
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-10-24 22:51:02 +0200

    Allow to disable Modal's confirm button

--- ui/Modal.js
+++ ui/Modal.js
@@ -7,9 +7,10 @@ import Divider from './Divider'
 
 export default class Modal extends Component {
   render () {
-    const { title, confirmLabel, children } = this.props
-    const size = this.props.size || 'default'
+    const { title, children, disableConfirm } = this.props
+    const confirmLabel = this.props.confirmLabel || 'OK'
     const cancelLabel = this.props.cancelLabel || 'Cancel'
+    const size = this.props.size || 'default'
 
     const headerEl = $$('div', { class: 'se-modal-header' },
       $$(HorizontalStack, {},
@@ -28,7 +29,7 @@ export default class Modal extends Component {
         $$('div', { class: 'se-modal-footer' },
           $$(HorizontalStack, {},
             $$(Button, { size: 'default', style: 'secondary', action: 'cancel' }, cancelLabel),
-            $$(Button, { size: 'default', style: 'primary', action: 'confirm' }, confirmLabel)
+            $$(Button, { size: 'default', style: 'primary', action: 'confirm', disabled: disableConfirm }, confirmLabel)
           )
         )
       )
