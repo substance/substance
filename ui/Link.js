7529ddbe19f378d4a3461449d2175bc6c4ef13c3
Michael Aufreiter <michael@substance.io>
2019-11-19 20:32:33 +0100

    Configure link as inverted or inline block.

--- ui/Link.js
+++ ui/Link.js
@@ -1,13 +1,20 @@
-import { $$ } from '../dom'
+import { $$ } from 'substance'
 
 export default function Link (props) {
   const el = $$('a').addClass('sc-link')
-  const style = props.style
+  const { style, inlineBlock, inverted } = props
   if (style) {
     el.addClass('sm-style-' + style)
   }
+  if (inlineBlock) {
+    el.addClass('sm-inline-block')
+  }
+  if (inverted) {
+    el.addClass('sm-inverted')
+  }
   const children = props.children
   const attributes = Object.assign({}, props)
+  delete attributes.style
   delete attributes.children
   el.attr(attributes)
   el.append(children)
