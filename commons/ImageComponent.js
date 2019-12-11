e035e906c43d602758b730a6b69077caf98c918c
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-12-11 16:12:50 +0100

    Improve ImageComponent.

--- commons/ImageComponent.js
+++ commons/ImageComponent.js
@@ -3,18 +3,24 @@ import NodeComponent from './NodeComponent'
 
 export default class ImageComponent extends NodeComponent {
   render () {
-    const node = this.props.node
+    const { errored } = this.state
+    const { node, placeholder } = this.props
     const urlResolver = this.context.urlResolver
     let url = node.src
     if (urlResolver) {
-      url = urlResolver.resolveUrl(url)
+      url = urlResolver.resolveUrl(url) || url
     }
-    const el = $$('img', { class: 'sc-image', 'data-id': node.id, src: url })
-    el.on('error', this._onError)
-    if (this.state.errored) {
-      el.attr({
-        title: 'Could not load image.'
-      })
+    const el = $$('img', { class: 'sc-image', 'data-id': node.id, onerror: this._onError })
+    if (url) {
+      el.setAttribute('src', url)
+    } else {
+      if (placeholder) {
+        el.setAttribute('src', placeholder)
+      }
+      el.addClass('sm-empty')
+    }
+    if (errored) {
+      el.attr({ title: 'Could not load image.' })
       el.addClass('sm-error')
     }
     return el
