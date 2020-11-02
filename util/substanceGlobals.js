44fa14f77545e9bfc87cad90f6c7c26d0bbeb90c
Oliver Buchtala <oliver.buchtala@gmail.com>
2020-11-02 09:28:12 +0100

    Allow to use substanceGlobals in a worker.

--- util/substanceGlobals.js
+++ util/substanceGlobals.js
@@ -1,8 +1,9 @@
+/* global self */
 import _isDefined from './_isDefined'
 
 /**
   A place to store global variables.
 */
-const _global = (typeof global !== 'undefined') ? global : window
+const _global = (typeof global !== 'undefined') ? global : (typeof window !== 'undefined') ? window : (typeof self !== 'undefined') ? self : undefined
 const substanceGlobals = _isDefined(_global.Substance) ? _global.Substance : _global.Substance = {}
 export default substanceGlobals
